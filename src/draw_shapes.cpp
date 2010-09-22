7e13ab9f9f4b862494cd822eff49ddf41d5d13a5
Frank <Frank@5d77776e-c07b-11df-90e9-6755bb0d239d>
2010-09-22 17:39:14 +0000

    Enabled dlists for back facing specular objects because they can't have specular reflections.

--- src/draw_shapes.cpp
+++ src/draw_shapes.cpp
@@ -998,8 +998,8 @@ void draw_quad_tri(point const *pts0, vector3d const *normals0, int npts, int di
 	// 4. Must not be part of a quadric (too many LODs)
 	float const spec[2] = {c_obj.cp.specular, c_obj.cp.shine};
 	bool const is_specular(ALLOW_SPECULAR && spec[0] > 0.0 && !back_facing && all_lighted != ALL_LT[1]);
-	bool const use_dlist(USE_DLIST && !first_render && !no_shadow_calc && !is_specular && !q.is_quadric && !dg_lights &&
-		!has_d_shad && !has_smoke(pts, npts) && !has_dynamic_lights(pts, npts));
+	bool const use_dlist(USE_DLIST && !first_render && !no_shadow_calc && (!is_specular || back_facing) && !q.is_quadric &&
+		!dg_lights && !has_d_shad && !has_smoke(pts, npts) && !has_dynamic_lights(pts, npts));
 	bool const no_subdiv(no_shadow_edge || is_black);
 	unsigned lod_level(1);
 	float lod_scale(1.0);
