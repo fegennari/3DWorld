f44c1a64bcff4379aa1827e49ac9b557e5fc630f
Frank E. Gennari <Frank E. Gennari@5d77776e-c07b-11df-90e9-6755bb0d239d>
2011-07-05 15:32:14 +0000

    Fixed the header prototype for cobj tree intersection. -FG

--- src/3DWorld.h
+++ src/3DWorld.h
@@ -1351,9 +1351,11 @@ int  check_legal_move(int x_new, int y_new, float zval, float radius, int &cinde
 // function prototypes - coll_cell_search
 void build_cobj_tree( bool dynamic=0, bool verbose=1);
 void update_cobj_tree(bool dynamic=0, bool verbose=1);
-bool check_coll_line_exact_tree(point const &p1, point const &p2, point &cpos, vector3d &cnorm, int &cindex, int ignore_cobj, bool dynamic=0, int test_alpha=0);
+bool check_coll_line_exact_tree(point const &p1, point const &p2, point &cpos, vector3d &cnorm,
+	int &cindex, int ignore_cobj, bool dynamic=0, int test_alpha=0);
 bool check_coll_line_tree(point const &p1, point const &p2, int &cindex, int ignore_cobj, bool dynamic=0, int test_alpha=0);
-void get_intersecting_cobjs_tree(cube_t const &cube, vector<unsigned> &cobjs, int ignore_cobj, float toler, bool dynamic);
+void get_intersecting_cobjs_tree(cube_t const &cube, vector<unsigned> &cobjs, int ignore_cobj, float toler,
+	bool dynamic, bool check_ccounter, int id_for_cobj_int);
 bool check_vert_collision_sphere(point const &pos, float radius, int skip_dynamic, bool trans_test, vector<int> *cobjs=NULL);
 bool check_coll_line(point pos1, point pos2, int &cindex, int c_obj, int skip_dynamic, int test_alpha, bool no_tree=0);
 bool check_coll_line_exact(point pos1, point pos2, point &cpos, vector3d &coll_norm, int &cindex, float splash_val=0.0,
