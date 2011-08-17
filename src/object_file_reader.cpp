c5a324276369e37cca81a5502f3ee38de1493ab6
Frank E. Gennari <Frank E. Gennari@5d77776e-c07b-11df-90e9-6755bb0d239d>
2011-08-17 05:12:46 +0000

    Split the object file reader up into a simple geometry reader and a material/model reader and started writing a model3d class. -FG

--- src/object_file_reader.cpp
+++ src/object_file_reader.cpp
@@ -10,44 +10,16 @@ using namespace std;
 
 class object_file_reader {
 
-	string filename, rel_path;
+protected:
+	string filename;
 	ifstream in;
-
-	struct material_t {
-		texture_t texture;
-		// FIXME: write
-	};
-	vector<material_t> materials;
-	typedef map<string, unsigned> mat_map_t;
-	mat_map_t mat_map; // maps material names to materials
 
 	bool open_file() { // FIXME: what about multiple opens/reads?
 		assert(!filename.empty());
 		in.open(filename);
 		if (in.good()) return 1;
-		std::cerr << "Error: Could not open object file " << filename << endl;
+		cerr << "Error: Could not open object file " << filename << endl;
 		return 0;
-	}
-
-	string open_include_file(string const &fn, string const &type, ifstream &in_inc) const {
-		assert(!fn.empty());
-		in_inc.open(fn); // try absolute path
-		if (in_inc.good()) return fn;
-		in_inc.clear();
-		string const rel_fn(rel_path + fn);
-		in_inc.open(rel_fn); // try relative path
-		if (in_inc.good()) return rel_fn;
-		std::cerr << "Error: Could not open " << type << " file " << fn << " or " << rel_fn << endl;
-		return string();
-	}
-
-	string get_path(string const &fn) const {
-		for (unsigned pos = fn.size(); pos > 0; --pos) {
-			if (fn[pos-1] == '\\' || fn[pos-1] == '/') {
-				return string(fn.begin(), fn.begin()+pos);
-			}
-		}
-		return string();
 	}
 
 	void read_to_newline(ifstream &in_) const {
@@ -68,26 +40,196 @@ class object_file_reader {
 		}
 	}
 
+public:
+	object_file_reader(string const &fn) : filename(fn) {assert(!fn.empty());}
+
+	bool read(vector<vector<point> > *ppts, bool verbose) {
+		RESET_TIME;
+		if (!open_file()) return 0;
+		vector<point> v; // vertices
+		string s;
+
+		while (in.good() && (in >> s)) {
+			assert(!s.empty());
+
+			if (s[0] == '#') { // comment
+				read_to_newline(in); // ignore
+			}
+			else if (s == "v") { // vertex
+				v.push_back(point());
+			
+				if (!(in >> v.back().x >> v.back().y >> v.back().z)) {
+					cerr << "Error reading vertex from object file " << filename << endl;
+					return 0;
+				}
+			}
+			else if (s == "f") { // face
+				if (ppts) ppts->push_back(vector<point>());
+				int ix(0);
+
+				while (in >> ix) { // read vertex index
+					normalize_index(ix, v.size());
+					if (ppts) ppts->back().push_back(v[ix]);
+
+					if (in.get() == '/') {
+						if (in >> ix) {} else in.clear(); // text coord index
+
+						if (in.get() == '/') {
+							if (in >> ix) {} else in.clear(); // normal index
+						}
+						else in.unget();
+					}
+					else in.unget();
+				}
+				in.clear();
+			}
+			else {
+				read_to_newline(in); // ignore everything else
+			}
+		}
+		PRINT_TIME("Polygons Load");
+		if (verbose) cout << "v: " << v.size() << ", f: " << (ppts ? ppts->size() : 0) << endl;
+		return 1;
+	}
+};
+
+
+// ************************************************
+
+
+typedef vector<vert_norm_tc> polygon_t;
+
+
+struct poly_group {
+	vector<polygon_t> polygons;
+};
+
+
+struct material_t {
+
+	texture_t texture;
+	// FIXME: write
+
+	// geometry - does this go here or somewhere else?
+	poly_group geom;
+};
+
+
+class model3d {
+
+	poly_group unbound_geom;
+	vector<material_t> materials;
+	typedef map<string, unsigned> mat_map_t;
+	mat_map_t mat_map; // maps material names to materials
+	set<string> undef_materials; // to reduce warning messages
+
+public:
+	unsigned num_materials(void) const {return materials.size();}
+
+	material_t &get_material(int mat_id) {
+		assert(mat_id >= 0 && (unsigned)mat_id < materials.size());
+		return materials[mat_id];
+	}
+
+	void add_polygon(polygon_t const &poly, int mat_id) {
+		//assert(mat_id >= 0); // must be set/valid - FIXME: too strict?
+
+		if (mat_id < 0) {
+			unbound_geom.polygons.push_back(poly);
+		}
+		else {
+			assert((unsigned)mat_id < materials.size());
+			materials[mat_id].geom.polygons.push_back(poly);
+		}
+	}
+
+	int get_material_ix(string const &material_name, string const &fn) {
+		unsigned mat_id(0);
+		mat_map_t::const_iterator it(mat_map.find(material_name));
+
+		if (it == mat_map.end()) {
+			mat_id = materials.size();
+			mat_map[material_name] = mat_id;
+			materials.push_back(material_t());
+		}
+		else {
+			cerr << "Warning: Redefinition of material " << material_name << " in file " << fn << endl;
+			mat_id = it->second;
+		}
+		assert(mat_id < materials.size());
+		return mat_id;
+	}
+
+	int find_material(string const &material_name) {
+		mat_map_t::const_iterator it(mat_map.find(material_name));
+
+		if (it == mat_map.end()) {
+			if (undef_materials.find(material_name) == undef_materials.end()) {
+				cerr << "Error: Material " << material_name << " not found in any included material libraries" << endl;
+				undef_materials.insert(material_name);
+			}
+			return -1; // return -1 on failure
+		}
+		assert(it->second < materials.size());
+		return it->second;
+	}
+
 	bool load_texture(string const &fn, unsigned mat_id) {
 		assert(mat_id < materials.size());
-		assert(!materials[mat_id].texture.data); // should not yet be loaded - FIXME: too strict?
-		materials[mat_id].texture.free();
+		material_t &mat(materials[mat_id]);
+		assert(!mat.texture.data); // should not yet be loaded - FIXME: too strict?
+		mat.texture.free();
+		// type format width height wrap ncolors use_mipmaps name [bump_name [id [color]]]
+		mat.texture = texture_t(0, 4, 0, 0, 1, 3, 1, fn); // always RGB targa wrapped+mipmap
+		mat.texture.load(-1);
+		return 1; // can't fail
+	}
+};
+
+
+// ************************************************
+
+
+class object_file_reader_model : public object_file_reader {
+
+	string rel_path;
+	model3d &model;
+
+	string open_include_file(string const &fn, string const &type, ifstream &in_inc) const {
+		assert(!fn.empty());
+		in_inc.open(fn); // try absolute path
+		if (in_inc.good()) return fn;
+		in_inc.clear();
+		string const rel_fn(rel_path + fn);
+		in_inc.open(rel_fn); // try relative path
+		if (in_inc.good()) return rel_fn;
+		cerr << "Error: Could not open " << type << " file " << fn << " or " << rel_fn << endl;
+		return string();
+	}
+
+	string get_path(string const &fn) const {
+		for (unsigned pos = fn.size(); pos > 0; --pos) {
+			if (fn[pos-1] == '\\' || fn[pos-1] == '/') {
+				return string(fn.begin(), fn.begin()+pos);
+			}
+		}
+		return string();
+	}
+
+	bool load_texture(string const &fn, unsigned mat_id) {
 		ifstream tex_in; // used only for determining file location
 		string const fn_used(open_include_file(fn, "texture", tex_in));
 		if (fn_used.empty()) return 0;
 		in.close();
 		cout << "loading texture " << fn_used << endl; // FIXME: too verbose?
-		// type format width height wrap ncolors use_mipmaps name [bump_name [id [color]]]
-		materials[mat_id].texture = texture_t(0, 4, 0, 0, 1, 3, 1, fn_used); // always RGB targa wrapped+mipmap
-		materials[mat_id].texture.load(-1);
-		return 1;
+		return model.load_texture(fn_used, mat_id);
 	}
 
 	bool load_mat_lib(string const &fn) { // FIXME: cache these files or reload every time?
 		ifstream mat_in;
 		if (open_include_file(fn, "material library", mat_in).empty()) return 0;
 		cout << "loading material library " << fn << endl;
-		int cur_mat(-1); // not set
+		int cur_mat_id(-1); // not set
 		string s;
 
 		while (mat_in.good() && (mat_in >> s)) {
@@ -97,25 +239,14 @@ class object_file_reader {
 				read_to_newline(mat_in); // ignore
 			}
 			else if (s == "newmtl") { // new material
-				string material;
+				string material_name;
 
-				if (!(mat_in >> material)) {
-					std::cerr << "Error reading material name" << endl;
+				if (!(mat_in >> material_name)) {
+					cerr << "Error reading material name" << endl;
 					return 0;
 				}
-				cout << "Material " << material << endl; // FIXME: too verbose?
-				mat_map_t::const_iterator it(mat_map.find(material));
-
-				if (it == mat_map.end()) {
-					cur_mat = materials.size();
-					mat_map[material] = cur_mat;
-					materials.push_back(material_t());
-				}
-				else {
-					std::cerr << "Warning: Redefinition of material " << material << " in file " << fn << endl;
-					cur_mat = it->second;
-				}
-				assert((unsigned)cur_mat < materials.size());
+				cout << "Material " << material_name << endl; // FIXME: too verbose?
+				cur_mat_id = model.get_material_ix(material_name, fn);
 			}
 			/*else if (s == "Ka") {
 
@@ -163,7 +294,7 @@ class object_file_reader {
 
 			}*/
 			else {
-				//std::cerr << "Error: Undefined entry '" << s << "' in material library" << endl;
+				//cerr << "Error: Undefined entry '" << s << "' in material library" << endl;
 				//return 0;
 				read_to_newline(mat_in); // ignore
 			}
@@ -172,19 +303,18 @@ class object_file_reader {
 	}
 
 public:
-	object_file_reader(string const &fn) : filename(fn) {
-		assert(!fn.empty());
+	object_file_reader_model(string const &fn, model3d &model_) : object_file_reader(fn), model(model_) {
 		rel_path = get_path(fn);
 	}
 
-	bool read_to_cxx_obj(vector<vector<vert_norm_tc> > &data, bool verbose) {
+	bool read(vector<vector<point> > *ppts, bool verbose) {
 		RESET_TIME;
 		if (!open_file()) return 0;
 		int cur_mat_id(-1);
 		vector<point> v; // vertices
 		vector<vector3d> n; // normals
 		vector<vector3d> tc; // texture coords
-		string s, group_name, material, mat_lib, last_mat_lib;
+		string s, group_name, material_name, mat_lib, last_mat_lib;
 
 		while (in.good() && (in >> s)) {
 			assert(!s.empty());
@@ -193,11 +323,13 @@ public:
 				read_to_newline(in); // ignore
 			}
 			else if (s == "f") { // face
-				data.push_back(vector<vert_norm_tc>());
+				if (ppts) ppts->push_back(vector<point>());
+				polygon_t poly;
 				int vix(0), tix(0), nix(0);
 
 				while (in >> vix) { // read vertex index
 					normalize_index(vix, v.size());
+					if (ppts) ppts->back().push_back(v[vix]);
 					vector3d normal;
 					vector3d tex_coord;
 
@@ -227,17 +359,16 @@ public:
 					else {
 						in.unget();
 					}
-					assert(cur_mat_id >= 0); // must be set/valid - FIXME: too strict?
-					// FIXME: use cur_mat_id
-					data.back().push_back(vert_norm_tc(v[vix], normal, tex_coord.x, tex_coord.y));
-				} // end while
+					poly.push_back(vert_norm_tc(v[vix], normal, tex_coord.x, tex_coord.y));
+				} // end while vertex
 				in.clear();
+				model.add_polygon(poly, cur_mat_id);
 			}
 			else if (s == "v") { // vertex
 				v.push_back(point());
 			
 				if (!(in >> v.back().x >> v.back().y >> v.back().z)) {
-					std::cerr << "Error reading vertex from object file " << filename << endl;
+					cerr << "Error reading vertex from object file " << filename << endl;
 					return 0;
 				}
 			}
@@ -245,7 +376,7 @@ public:
 				tc.push_back(vector3d());
 			
 				if (!(in >> tc.back().x >> tc.back().y)) {
-					std::cerr << "Error reading texture coord from object file " << filename << endl;
+					cerr << "Error reading texture coord from object file " << filename << endl;
 					return 0;
 				}
 				if (!(in >> tc.back().z)) in.clear(); // optionally read a third tex coord
@@ -254,7 +385,7 @@ public:
 				n.push_back(vector3d());
 			
 				if (!(in >> n.back().x >> n.back().y >> n.back().z)) {
-					std::cerr << "Error reading normal from object file " << filename << endl;
+					cerr << "Error reading normal from object file " << filename << endl;
 					return 0;
 				}
 			}
@@ -266,7 +397,7 @@ public:
 			}
 			else if (s == "g") { // group
 				if (!(in >> group_name)) {
-					std::cerr << "Error reading group name from object file " << filename << endl;
+					cerr << "Error reading group name from object file " << filename << endl;
 					return 0;
 				}
 			}
@@ -274,85 +405,33 @@ public:
 				read_to_newline(in); // ignore
 			}
 			else if (s == "usemtl") { // use material
-				if (!(in >> material)) {
-					std::cerr << "Error reading material from object file " << filename << endl;
+				if (!(in >> material_name)) {
+					cerr << "Error reading material from object file " << filename << endl;
 					return 0;
 				}
-				mat_map_t::const_iterator it(mat_map.find(material));
-
-				if (it == mat_map.end()) {
-					std::cerr << "Error: Material " << material << " not found in any included material libraries" << endl;
-					return 0;
-				}
-				cur_mat_id = it->second;
-				assert((unsigned)cur_mat_id < materials.size());
+				cur_mat_id = model.find_material(material_name);
 			}
 			else if (s == "mtllib") { // material library
 				if (!(in >> mat_lib)) {
-					std::cerr << "Error reading material library from object file " << filename << endl;
+					cerr << "Error reading material library from object file " << filename << endl;
 					return 0;
 				}
 				if (mat_lib != last_mat_lib && !load_mat_lib(mat_lib)) {
-					std::cerr << "Error reading material library file " << mat_lib << endl;
+					cerr << "Error reading material library file " << mat_lib << endl;
 					return 0;
 				}
 			}
 			else {
-				std::cerr << "Error: Undefined entry '" << s << "' in object file " << filename << endl;
+				cerr << "Error: Undefined entry '" << s << "' in object file " << filename << endl;
 				return 0;
 			}
 		}
 		PRINT_TIME("CXX Object Load");
-		if (verbose) cout << "v: " << v.size() << ", n: " << n.size() << ", tc: " << tc.size() << ", f: " << data.size() << endl;
-		return 1;
-	}
-
-	bool read_to_points_vector(vector<vector<point> > &ppts, bool verbose) {
-		RESET_TIME;
-		if (!open_file()) return 0;
-		vector<point> v; // vertices
-		string s;
-
-		while (in.good() && (in >> s)) {
-			assert(!s.empty());
-
-			if (s[0] == '#') { // comment
-				read_to_newline(in); // ignore
-			}
-			else if (s == "v") { // vertex
-				v.push_back(point());
-			
-				if (!(in >> v.back().x >> v.back().y >> v.back().z)) {
-					std::cerr << "Error reading vertex from object file " << filename << endl;
-					return 0;
-				}
-			}
-			else if (s == "f") { // face
-				ppts.push_back(vector<point>());
-				int ix(0);
-
-				while (in >> ix) { // read vertex index
-					normalize_index(ix, v.size());
-					ppts.back().push_back(v[ix]);
-
-					if (in.get() == '/') {
-						if (in >> ix) {} else in.clear(); // text coord index
-
-						if (in.get() == '/') {
-							if (in >> ix) {} else in.clear(); // normal index
-						}
-						else in.unget();
-					}
-					else in.unget();
-				}
-				in.clear();
-			}
-			else {
-				read_to_newline(in); // ignore everything else
-			}
+		
+		if (verbose) {
+			cout << "v: " << v.size() << ", n: " << n.size() << ", tc: " << tc.size()
+				 << ", f: " << (ppts ? ppts->size() : 0) << ", mat: " << model.num_materials() << endl;
 		}
-		PRINT_TIME("Polygons Load");
-		if (verbose) cout << "v: " << v.size() << ", f: " << ppts.size() << endl;
 		return 1;
 	}
 };
@@ -360,15 +439,15 @@ public:
 
 bool read_object_file(char *filename, vector<vector<point> > &ppts, bool verbose) {
 
-	{ // BEGIN TESTING
+	if (1) {
+		model3d model;
+		object_file_reader_model reader(filename, model);
+		return reader.read(&ppts, verbose);
+	}
+	else {
 		object_file_reader reader(filename);
-		vector<vector<vert_norm_tc> > data;
-		cout << "success: " << reader.read_to_cxx_obj(data, verbose) << endl;
-	} // END TESTING
-
-	object_file_reader reader(filename);
-	return reader.read_to_points_vector(ppts, verbose);
-	
+		return reader.read(&ppts, verbose);
+	}
 }
 
 
