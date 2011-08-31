096fa389713fe6ff6b93bd532be259bdc62927f4
Frank E. Gennari <Frank E. Gennari@5d77776e-c07b-11df-90e9-6755bb0d239d>
2011-08-31 05:48:46 +0000

    Created source and buffer classes for OpenAL. -FG

--- src/openal_wrap.cpp
+++ src/openal_wrap.cpp
@@ -12,82 +12,80 @@
 using namespace std;
 
 
-void load_wave_file(string const &fn, unsigned buffer) {
-
-#if 0 // old (deprecated) version
-	ALsizei size, freq;
-	ALenum format;
-	ALboolean loop;
-	ALvoid *data;
-	alutLoadWAVFile((char *)fn.c_str(), &format, &data, &size, &freq, &loop);
-    alBufferData(buffer, format, data, size, freq);
-    alutUnloadWAV(format, data, size, freq);
-#else
-
-#endif
-}
-
-
-unsigned gen_openal_buffer() {
-
-	unsigned buffer(0);
-    alGenBuffers(1, &buffer);
-    
-    if (alGetError() != AL_NO_ERROR) {
-        cerr << "Error creating OpenAL buffers" << endl;
-        exit(1);
-    }
-	assert(buffer > 0);
-	return buffer;
-}
-
-
-unsigned gen_openal_source() {
-
-	unsigned source(0);
-    alGenSources(1, &source);
-
-    if (alGetError() != AL_NO_ERROR) {
-       cerr << "Error creating OpenAL sources" << endl;
-       exit(1);
-    }
-	assert(source > 0);
-	return source;
-}
-
-
-void free_openal_buffer(unsigned &buffer) {
-
-	assert(buffer > 0); // too strict?
-	alDeleteBuffers(1, &buffer);
-	buffer = 0;
-}
-
-
-void free_openal_source(unsigned &source) {
-
-	assert(source > 0); // too strict?
-	alDeleteSources(1, &source);
-	source = 0;
-}
-
-
-void setup_openal_source(unsigned source, unsigned buffer, point const &pos, vector3d const &vel, float pitch=1.0, float gain=1.0, bool looping=0) {
-
-	assert(source > 0 && buffer > 0);
-	alSourcef (source, AL_PITCH,    pitch);
-    alSourcef (source, AL_GAIN,     gain);
-    alSourcefv(source, AL_POSITION, &pos.x);
-    alSourcefv(source, AL_VELOCITY, &vel.x);
-    alSourcei (source, AL_BUFFER,   buffer);
-    alSourcei (source, AL_LOOPING,  looping);
-}
-
-
 struct openal_orient {
-	vector3d v1, v2;
+
+	vector3d at, up;
 	openal_orient() {}
-	openal_orient(vector3d const &v1_, vector3d const &v2_) : v1(v1_), v2(v2_) {}
+	openal_orient(vector3d const &at_, vector3d const &up_) : at(at_), up(up_) {}
+};
+
+
+class openal_buffer {
+
+	unsigned buffer;
+
+public:
+	openal_buffer() : buffer(0) {}
+	~openal_buffer() {free();}
+	bool is_valid() const {return (buffer > 0);}
+	unsigned get_buffer_ix() const {return buffer;}
+	
+	void alloc() {
+		 alGenBuffers(1, &buffer);
+    
+		if (alGetError() != AL_NO_ERROR) {
+			cerr << "Error creating OpenAL buffers" << endl;
+			exit(1);
+		}
+		assert(is_valid());
+	}
+	void free() {
+		if (is_valid()) {
+			alDeleteBuffers(1, &buffer);
+			buffer = 0;
+		}
+	}
+};
+
+
+class openal_source {
+
+	unsigned source;
+
+public:
+	openal_source() : source(0) {}
+	~openal_source() {free();}
+	bool is_valid() const {return (source > 0);}
+	
+	void alloc() {
+		alGenSources(1, &source);
+
+		if (alGetError() != AL_NO_ERROR) {
+		   cerr << "Error creating OpenAL sources" << endl;
+		   exit(1);
+		}
+		assert(is_valid());
+	}
+	void free() {
+		if (is_valid()) {
+			alDeleteSources(1, &source);
+			source = 0;
+		}
+	}
+	void setup(openal_buffer const &buffer, point const &pos, vector3d const &vel, float pitch=1.0, float gain=1.0, bool looping=0) {
+		assert(is_valid() && buffer.is_valid());
+		alSourcef (source, AL_PITCH,    pitch);
+		alSourcef (source, AL_GAIN,     gain);
+		alSourcefv(source, AL_POSITION, &pos.x);
+		alSourcefv(source, AL_VELOCITY, &vel.x);
+		alSourcei (source, AL_LOOPING,  looping);
+		set_buffer_ix(buffer.get_buffer_ix());
+	}
+	void set_buffer_ix(unsigned buffer_ix) {alSourcei(source, AL_BUFFER, buffer_ix);}
+	void play()   const {alSourcePlay  (source);}
+	void stop()   const {alSourceStop  (source);}
+	void pause()  const {alSourcePause (source);}
+	void rewind() const {alSourceRewind(source);}
 };
 
 
@@ -95,7 +93,23 @@ void setup_openal_listener(point const &pos, vector3d const &vel, openal_orient 
 
 	alListenerfv(AL_POSITION,    &pos.x);
     alListenerfv(AL_VELOCITY,    &vel.x);
-    alListenerfv(AL_ORIENTATION, &orient.v1.x);
+    alListenerfv(AL_ORIENTATION, &orient.at.x);
+}
+
+
+void load_wave_file(string const &fn, openal_buffer &buffer) {
+
+#if 0 // old (deprecated) version
+	ALsizei size, freq;
+	ALenum format;
+	ALboolean loop;
+	ALvoid *data;
+	alutLoadWAVFile((char *)fn.c_str(), &format, &data, &size, &freq, &loop);
+    alBufferData(buffer.get_buffer_ix(), format, data, size, freq);
+    alutUnloadWAV(format, data, size, freq);
+#else
+
+#endif
 }
 
 
@@ -107,25 +121,25 @@ void run_openal_test() {
 	point    const source_pos(-2.0, 0.0, 0.0);
 	vector3d const source_vel(0.0, 0.0, 0.0);
 	setup_openal_listener(listen_pos, listen_vel, listen_orient);
-	
-	unsigned buffer(gen_openal_buffer());
-	unsigned source(gen_openal_source());
+	openal_buffer buffer;
+	openal_source source;
+	buffer.alloc();
+	source.alloc();
 	load_wave_file("test.wav", buffer);
-	setup_openal_source(source, buffer, source_pos, source_vel, 1.0, 1.0, 0);
+	source.setup(buffer, source_pos, source_vel, 1.0, 1.0, 0);
 	// write
-	free_openal_buffer(buffer);
-	free_openal_source(source);
 }
 
 
 void openal_hello_world() {
 
 	unsigned buffer(alutCreateBufferHelloWorld());
-	unsigned source(gen_openal_source());
-	alSourcei(source, AL_BUFFER, buffer);
-	alSourcePlay(source);
+	openal_source source;
+	source.alloc();
+	source.set_buffer_ix(buffer);
+	source.play();
+	alutSleep(1.0);
 	//free_openal_buffer(buffer);
-	//free_openal_source(source);
 }
 
 
@@ -133,7 +147,8 @@ void init_openal(int &argc, char** argv) {
 
 	return; // not yet ready
 	alutInit(&argc, argv);
-	//openal_hello_world();
+	alGetError(); // ignore any previous errors
+	openal_hello_world();
 }
 
 
