dd24edc8a0634be716ee354b696c60170b931229
Frank E. Gennari <Frank E. Gennari@5d77776e-c07b-11df-90e9-6755bb0d239d>
2011-10-27 03:21:10 +0000

    More work on cobj_tree_triangles_t. Split cobj_tree_base out as a base class. Modified cobj_tree_t so that it's more robust to degenerate cobjs. -FG

--- src/cobj_bsp_tree.cpp
+++ src/cobj_bsp_tree.cpp
@@ -16,17 +16,16 @@ extern vector<unsigned> falling_cobjs;
 extern platform_cont platforms;
 
 
-struct coll_triangle : public triangle { // size = 64
+struct coll_triangle : public triangle { // size = 52
+
 	vector3d normal;
-	colorRGBA color;
+	unsigned cid;
 
 	coll_triangle() {}
+	coll_triangle(coll_obj const &c) : triangle(c.points), normal(c.norm), cid(c.id) {assert(is_cobj_valid(c));}
 
-	coll_triangle(coll_obj const &c) : triangle(c.points), normal(c.norm), color(c.get_avg_color()) {
-		assert(c.status == COLL_STATIC || c.status == COLL_DYNAMIC);
-		assert(c.type == COLL_POLYGON);
-		assert(c.npoints == 3);
-		assert(c.thickness <= MIN_POLY_THICK);
+	static bool is_cobj_valid(coll_obj const &c) {
+		return (!c.disabled() && c.type == COLL_POLYGON && c.npoints == 3 && c.thickness <= MIN_POLY_THICK);
 	}
 	cube_t get_bounding_cube() const {
 		return cube_t(min(pts[0].x, min(pts[1].x, pts[2].x)), max(pts[0].x, max(pts[1].x, pts[2].x)),
@@ -46,21 +45,211 @@ struct coll_triangle : public triangle { // size = 64
 };
 
 
-// 3: BSP Tree/KD-Tree, 8: OctTree
-template<unsigned NUM> class cobj_tree_t {
+struct tree_coll_triangle : public coll_triangle { // size = 56
+	unsigned char bix;
+	tree_coll_triangle() {}
+	tree_coll_triangle(coll_obj const &c) : coll_triangle(c) {}
+	bool operator<(tree_coll_triangle const &t) const {return (bix < t.bix);} // sort by bix
+};
+
+
+class cobj_tree_base {
+
 protected:
 	struct tree_node : public cube_t { // size = 36
 		unsigned start, end; // index into cixs for leaves
 		unsigned next_node_id;
+		//unsigned char split_dim;
 
 		tree_node(unsigned s=0, unsigned e=0) : start(s), end(e), next_node_id(0) {
 			UNROLL_3X(d[i_][0] = d[i_][1] = 0.0;)
 		}
 	};
 
+	vector<tree_node> nodes;
+
+	struct node_ix_mgr {
+		point const p1;
+		vector3d dinv;
+		vector<tree_node> const &nodes;
+
+		node_ix_mgr(vector<tree_node> const &nodes_, point const &p1_, point const &p2_)
+			: nodes(nodes_), p1(p1_), dinv(p2_ - p1_) {dinv.invert(0, 1);}
+
+		bool check_node(unsigned &nix) const {
+			tree_node const &n(nodes[nix]);
+
+			if (!get_line_clip2(p1, dinv, n.d)) {
+				assert(n.next_node_id > nix);
+				nix = n.next_node_id; // failed the bbox test
+				assert(nix > 0);
+				return 0;
+			}
+			++nix;
+			return 1;
+		}
+	};
+
+public:
+	bool is_empty() const {return nodes.empty();}
+	void clear() {nodes.resize(0);}
+};
+
+
+class cobj_tree_triangles_t : public cobj_tree_base {
+
+	vector<tree_coll_triangle> tris;
+
+	void calc_node_bbox(tree_node &n) const {
+		assert(n.start < n.end && n.end <= tris.size());
+		n.copy_from(tris[n.start].get_bounding_cube());
+
+		for (unsigned i = n.start+1; i < n.end; ++i) { // bbox union
+			n.union_with_cube(tris[i].get_bounding_cube());
+		}
+	}
+
+	void build_tree(unsigned nix, unsigned skip_dims) {
+		assert(nix < nodes.size());
+		tree_node &n(nodes[nix]);
+		assert(n.start < n.end);
+		calc_node_bbox(n);
+		unsigned const num(n.end - n.start);
+		if (num <= MAX_LEAF_SIZE || skip_dims == 7) return; // base case
+	
+		// determine split dimension and value
+		unsigned dim(0);
+		float max_sz(0);
+
+		for (unsigned d = 0; d < 3; ++d) {
+			if (skip_dims & (1 << d)) continue;
+			float const dim_sz(n.d[d][1] - n.d[d][0]);
+			assert(dim_sz >= 0.0);
+		
+			if (dim_sz > max_sz) {
+				max_sz = dim_sz;
+				dim    = d;
+			}
+		}
+		if (max_sz == 0) return; // can't split
+		assert(!(skip_dims & (1 << dim)));
+		float const sval(0.5*(n.d[dim][1] + n.d[dim][0])); // center point (mean seems to work better than median)
+
+		// split in this dimension
+		for (unsigned i = n.start; i < n.end; ++i) {
+			tris[i].bix = 2;
+			cube_t const cube(tris[i].get_bounding_cube());
+			assert(cube.d[dim][0] <= cube.d[dim][1]);
+			if (cube.d[dim][1] <= sval) tris[i].bix = 0; // ends   before the split, put in bin 0
+			if (cube.d[dim][0] >= sval) tris[i].bix = 1; // starts after  the split, put in bin 1
+		}
+		sort((tris.begin() + n.start), (tris.begin() + n.end)); // sort by bix then by ix
+		unsigned bin_count[3] = {0};
+
+		for (unsigned i = n.start; i < n.end; ++i) {
+			assert(tris[i].bix < 3);
+			++bin_count[tris[i].bix];
+		}
+
+		// check that dataset has been subdivided (not all in one bin)
+		if (bin_count[0] == num || bin_count[1] == num || bin_count[2] == num) {
+			build_tree(nix, (skip_dims | (1 << dim))); // single bin, rebin with a different dim
+			return;
+		}
+
+		// create child nodes and call recursively
+		unsigned cur(n.start);
+
+		for (unsigned bix = 0; bix < 3; ++bix) { // Note: this loop will invalidate the reference to 'n'
+			unsigned const count(bin_count[bix]);
+			if (count == 0) continue; // empty bin
+			unsigned const kid(nodes.size());
+			nodes.push_back(tree_node(cur, cur+count));
+			build_tree(kid, skip_dims);
+			nodes[kid].next_node_id = nodes.size();
+			cur += count;
+		}
+		tree_node &n2(nodes[nix]); // create a new reference
+		assert(cur == n2.end);
+		n2.start = n2.end = 0; // branch node has no leaves
+	}
+
+
+public:
+	void clear() {
+		cobj_tree_base::clear();
+		tris.clear(); // reserve(0)?
+	}
+
+	void add_cobjs(vector<coll_obj> const &cobjs, bool verbose) {
+		RESET_TIME;
+		clear();
+		tris.reserve(cobjs.size()); // is this a good idea?
+		
+		for (vector<coll_obj>::const_iterator i = cobjs.begin(); i != cobjs.end(); ++i) {
+			//if (i->disabled()) continue;
+			if (i->status != COLL_STATIC) continue;
+			assert(i->type == COLL_POLYGON && i->thickness <= MIN_POLY_THICK);
+
+			if (i->npoints == 4) { // quad => 2 triangles
+				coll_obj cobj(*i);
+				cobj.npoints = 3;
+				tris.push_back(tree_coll_triangle(cobj));
+				cobj.points[1] = cobj.points[2];
+				cobj.points[2] = cobj.points[3];
+				tris.push_back(tree_coll_triangle(cobj));
+			}
+			else {
+				tris.push_back(tree_coll_triangle(*i));
+			}
+		}
+		nodes.reserve(6*tris.size()/5); // conservative
+		nodes.push_back(tree_node(0, tris.size()));
+		assert(nodes.size() == 1);
+		build_tree(0, 0);
+		nodes[0].next_node_id = nodes.size();
+
+		if (verbose) {
+			PRINT_TIME(" Cobj Tree Triangles Create");
+			cout << "cobjs: " << cobjs.size() << ", leaves: " << tris.size() << ", nodes: " << nodes.size() << endl;
+		}
+	}
+
+	bool check_coll_line(point const &p1, point const &p2, point &cpos, vector3d &cnorm, int &cindex, int ignore_cobj, bool exact) const {
+		cindex = -1;
+		if (nodes.empty()) return 0;
+		bool ret(0);
+		float t(0.0), tmin(0.0), tmax(1.0);
+		node_ix_mgr nixm(nodes, p1, p2);
+
+		for (unsigned nix = 0; nix < nodes.size();) {
+			tree_node const &n(nodes[nix]);
+			if (!nixm.check_node(nix)) continue;
+
+			for (unsigned i = n.start; i < n.end; ++i) { // check leaves
+				// Note: we test cobj against the original (unclipped) p1 and p2 so that t is correct
+				// Note: we probably don't need to return cnorm and cpos in inexact mode, but it shouldn't be too expensive to do so
+				if ((int)tris[i].cid == ignore_cobj) continue;
+				if (!tris[i].line_int_exact(p1, p2, t, cnorm, tmin, tmax)) continue;
+				cindex = tris[i].cid;
+				cpos   = p1 + (p2 - p1)*t;
+				if (!exact) return 1; // return first hit
+				nixm.dinv = vector3d(cpos - p1);
+				nixm.dinv.invert(0, 1);
+				tmax = t;
+				ret  = 1;
+			}
+		}
+		return ret;
+	}
+}; // cobj_tree_triangles_t
+
+
+// 3: BSP Tree/KD-Tree, 8: OctTree
+template<unsigned NUM> class cobj_tree_t : public cobj_tree_base {
+
 	vector<coll_obj> const &cobjs;
 	vector<unsigned> cixs;
-	vector<tree_node> nodes;
 	bool is_static, is_dynamic, occluders_only, moving_only;
 
 	void add_cobj(unsigned ix)                         {if (obj_ok(cobjs[ix])) cixs.push_back(ix);}
@@ -113,10 +302,8 @@ public:
 	cobj_tree_t(vector<coll_obj> const &cobjs_, bool s, bool d, bool o, bool m)
 		: cobjs(cobjs_), is_static(s), is_dynamic(d), occluders_only(o), moving_only(m) {}
 
-	bool is_empty() const {return nodes.empty();}
-
 	void clear() {
-		nodes.resize(0);
+		cobj_tree_base::clear();
 		cixs.resize(0);
 	}
 
@@ -135,28 +322,6 @@ public:
 			cout << "cobjs: " << cobjs.size() << ", leaves: " << cixs.size() << ", nodes: " << nodes.size() << endl;
 		}
 	}
-
-	struct node_ix_mgr {
-		point const p1;
-		vector3d dinv;
-		vector<tree_node> const &nodes;
-
-		node_ix_mgr(vector<tree_node> const &nodes_, point const &p1_, point const &p2_)
-			: nodes(nodes_), p1(p1_), dinv(p2_ - p1_) {dinv.invert(0, 1);}
-
-		bool check_node(unsigned &nix) const {
-			tree_node const &n(nodes[nix]);
-
-			if (!get_line_clip2(p1, dinv, n.d)) {
-				assert(n.next_node_id > nix);
-				nix = n.next_node_id; // failed the bbox test
-				assert(nix > 0);
-				return 0;
-			}
-			++nix;
-			return 1;
-		}
-	};
 
 	bool check_coll_line(point const &p1, point const &p2, point &cpos, vector3d &cnorm, int &cindex, int ignore_cobj,
 		bool exact, int test_alpha, bool skip_non_drawn) const
@@ -282,6 +447,7 @@ template <> void cobj_tree_t<3>::build_tree(unsigned nix, unsigned skip_dims) {
 			dim    = d;
 		}
 	}
+	if (max_sz == 0) return; // can't split
 	assert(!(skip_dims & (1 << dim)));
 	float const sval(0.5*(n.d[dim][1] + n.d[dim][0])); // center point (mean seems to work better than median)
 
@@ -305,9 +471,7 @@ template <> void cobj_tree_t<3>::build_tree(unsigned nix, unsigned skip_dims) {
 	}
 
 	// check that dataset has been subdivided (not all in one bin)
-	assert(bin_count[0] < num && bin_count[1] < num);
-
-	if (bin_count[2] == num) {
+	if (bin_count[0] == num || bin_count[1] == num || bin_count[2] == num) {
 		build_tree(nix, (skip_dims | (1 << dim))); // single bin, rebin with a different dim
 		return;
 	}
@@ -384,6 +548,7 @@ cobj_tree_type cobj_tree_static (coll_objects, 1, 0, 0, 0);
 cobj_tree_type cobj_tree_dynamic(coll_objects, 0, 1, 0, 0);
 cobj_tree_type cobj_tree_occlude(coll_objects, 1, 0, 1, 0);
 cobj_tree_type cobj_tree_moving (coll_objects, 1, 0, 0, 1);
+cobj_tree_triangles_t cobj_tree_static_triangles;
 int last_update_frame[2] = {1, 1}; // first drawn frame is 1
 
 
