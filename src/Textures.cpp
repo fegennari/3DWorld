ba7258218ecd8a09dd5c43794bb6ef1f0606b71e
Frank E. Gennari <Frank E. Gennari@5d77776e-c07b-11df-90e9-6755bb0d239d>
2013-12-21 09:15:04 +0000

    Added some rock/sand/dirt/concrete normal map textures. The sand normal map can now be selected as a predefined texture. -FG

--- src/Textures.cpp
+++ src/Textures.cpp
@@ -146,7 +146,9 @@ texture_t(0, 5, 0,    0,    1, 3, 1, "foam1.jpg"), // 512x512
 
 texture_t(0, 5, 0,    0,    0, 3, 0, "bullet_hole/bullet_diffuse.jpg"), // 256x256
 texture_t(0, 5, 0,    0,    0, 1, 0, "bullet_hole/bullet_alpha.jpg"), // 256x256
-texture_t(0, 5, 0,    0,    0, 3, 0, "bullet_hole/bullet_normal.jpg") // 256x256
+texture_t(0, 5, 0,    0,    0, 3, 0, "bullet_hole/bullet_normal.jpg"), // 256x256
+
+texture_t(0, 5, 0,    0,    1, 3, 1, "normal_maps/sand_normal.jpg")
 //texture_t(0, 4, 0,    0,    1, 3, 1, "../Sponza2/textures/spnza_bricks_a_diff.tga")
 // type format width height wrap ncolors use_mipmaps name [invert_y [do_compress [anisotropy [mipmap_alpha_weight]]]]
 };
