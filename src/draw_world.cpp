8bcc852ea01397f041bfa6354cde8c8f8b0c5662
Frank E. Gennari <Frank E. Gennari@5d77776e-c07b-11df-90e9-6755bb0d239d>
2014-01-09 08:09:41 +0000

    Fixed compass so that it agrees with map mode by swapping N and S, and E and W. -FG

--- src/draw_world.cpp
+++ src/draw_world.cpp
@@ -1371,8 +1371,8 @@ void draw_compass_and_alt() { // and temperature
 	YELLOW.do_glColor();
 	sprintf(text, "Loc: (%3.2f, %3.2f, %3.2f)", (camera_origin.x+(xoff2-xoff)*DX_VAL), (camera_origin.y+(yoff2-yoff)*DY_VAL), camera_origin.z);
 	draw_text(-0.005*aspect_ratio, -0.01, -0.02, text);
-	float const theta(safe_acosf(-cview_dir.x)*TO_DEG);
-	int const octant(int(((cview_dir.y > 0) ? (360.0 - theta) : theta)/45.0 + 22.5)&7);
+	float const theta(safe_acosf(cview_dir.x)*TO_DEG);
+	int const octant(int(((cview_dir.y < 0) ? (360.0 - theta) : theta)/45.0 + 22.5)&7);
 	sprintf(text, "%s", dirs[octant].c_str());
 	draw_text(0.005*aspect_ratio, -0.01, -0.02, text);
 	sprintf(text, "Temp: %iC", int(temperature));
