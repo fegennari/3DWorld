70b885d6847d00ccdcb8bf91aeae81ba2edcc94d
Frank E. Gennari <Frank E. Gennari@5d77776e-c07b-11df-90e9-6755bb0d239d>
2014-03-16 22:52:56 +0000

    Moved draw_text() into its own file so that it can be implemented more efficiently in a way that uses shaders and not immediate mode. -FG

--- src/draw_world.cpp
+++ src/draw_world.cpp
@@ -1292,60 +1292,6 @@ void draw_splashes() {
 }
 
 
-void draw_text(colorRGBA const &color, float x, float y, float z, char const *text, float tsize, bool bitmap_font) {
-
-	//bitmap_font |= ((display_mode & 0x80) != 0);
-	shader_t s;
-	glDisable(GL_DEPTH_TEST);
-
-	if (bitmap_font) { // FIXME: uses fixed function pipeline, need to replace glRasterPos and glutBitmapCharacter with something else
-		color.do_glColor();
-		glRasterPos3f(x, y, z);
-	}
-	else {
-		s.begin_color_only_shader(color);
-		glEnable(GL_BLEND);
-		glEnable(GL_LINE_SMOOTH);
-		glPushMatrix();
-		glTranslatef(x, y, z);
-		uniform_scale(0.000005*tsize);
-	}
-	unsigned line_num(0);
-
-	while (*text) {
-		if (*text == '\n') { // newline (CR/LF)
-			++line_num;
-
-			if (bitmap_font) {
-				glRasterPos3f(x, y-(0.5*line_num)/window_height, z);
-			}
-			else {
-				glPopMatrix();
-				glPushMatrix();
-				glTranslatef(x, y-0.001*line_num*tsize, z);
-				uniform_scale(0.000005*tsize);
-			}
-		}
-		else {
-			if (bitmap_font) {
-				glutBitmapCharacter(GLUT_BITMAP_8_BY_13, *text); // other fonts available
-			}
-			else {
-				glutStrokeCharacter(GLUT_STROKE_ROMAN, *text); // GLUT_STROKE_MONO_ROMAN
-			}
-		}
-		text++;
-	}
-	if (!bitmap_font) {
-		glPopMatrix();
-		glDisable(GL_LINE_SMOOTH);
-		glDisable(GL_BLEND);
-		s.end_shader();
-	}
-	glEnable(GL_DEPTH_TEST);
-}
-
-
 void draw_framerate(float val) {
 
 	char text[32];
