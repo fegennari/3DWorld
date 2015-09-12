ca2810c31e02643bd68d28eef68099ecc0fc968d
Frank <Frank@5d77776e-c07b-11df-90e9-6755bb0d239d>
2015-09-12 20:23:15 +0000

    Better shadow mapf PCF offset calculation that depends on shadow map resolution.

--- src/3DWorld.cpp
+++ src/3DWorld.cpp
@@ -87,7 +87,7 @@ float mesh_file_scale(1.0), mesh_file_tz(0.0), speed_mult(1.0), mesh_z_cutoff(-F
 float water_h_off(0.0), water_h_off_rel(0.0), perspective_fovy(0.0), perspective_nclip(0.0), read_mesh_zmm(0.0), indir_light_exp(1.0);
 float snow_depth(0.0), snow_random(0.0), cobj_z_bias(DEF_Z_BIAS), init_temperature(DEF_TEMPERATURE), indir_vert_offset(0.25), sm_tree_density(1.0);
 float CAMERA_RADIUS(DEF_CAMERA_RADIUS), C_STEP_HEIGHT(0.6), waypoint_sz_thresh(1.0), model3d_alpha_thresh(0.9), model3d_texture_anisotropy(1.0), dist_to_fire_sq(0.0);
-float ocean_wave_height(DEF_OCEAN_WAVE_HEIGHT), tree_density_thresh(0.55), model_auto_tc_scale(0.0), model_triplanar_tc_scale(0.0), shadow_map_pcf_offset(0.01);
+float ocean_wave_height(DEF_OCEAN_WAVE_HEIGHT), tree_density_thresh(0.55), model_auto_tc_scale(0.0), model_triplanar_tc_scale(0.0), shadow_map_pcf_offset(0.0);
 float light_int_scale[NUM_LIGHTING_TYPES] = {1.0, 1.0, 1.0, 1.0};
 double camera_zh(0.0);
 point mesh_origin(all_zeros), camera_pos(all_zeros);
@@ -1904,9 +1904,7 @@ int load_config(string const &config_file) {
 		}
 		if (error) {cout << "Parse error in config file." << endl; break;}
 	} // while read
-	if (universe_only && disable_universe) {
-		cout << "Error: universe_only and disable_universe are mutually exclusive" << endl; error = 1;
-	}
+	if (universe_only && disable_universe) {cout << "Error: universe_only and disable_universe are mutually exclusive" << endl; error = 1;}
 	fclose(fp);
 	temperature    = init_temperature;
 	num_dodgeballs = max(num_dodgeballs, 1); // have to have at least 1
@@ -1914,6 +1912,7 @@ int load_config(string const &config_file) {
 	num_smileys    = max(num_smileys,    0);
 	teams          = max(teams,          1);
 	tree_mode      = tree_mode % 4;
+	if (shadow_map_sz > 0 && shadow_map_pcf_offset == 0.0) {shadow_map_pcf_offset = 40.0/shadow_map_sz;}
 	if (universe_only) world_mode = WMODE_UNIVERSE;
 	//if (read_heightmap && dynamic_mesh_scroll) cout << "Warning: read_heightmap and dynamic_mesh_scroll are currently incompatible options as the heightmap does not scroll." << endl;
 	DISABLE_WATER = INIT_DISABLE_WATER;
