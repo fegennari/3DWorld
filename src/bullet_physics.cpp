1eea71877c93bd93933c232bc5613590d0ad63d1
Frank <Frank@5d77776e-c07b-11df-90e9-6755bb0d239d>
2015-10-22 04:01:38 +0000

    Added Bullet physics libraries. May be temporary.

--- src/bullet_physics.cpp
+++ src/bullet_physics.cpp
@@ -4,12 +4,19 @@
 
 #include "3DWorld.h"
 #include "physics_objects.h"
-#include <btBulletCollisionCommon.h>
-#include <btBulletDynamicsCommon.h>
+
+#include "btBulletCollisionCommon.h"
+#include "btBulletDynamicsCommon.h"
+#include "Bullet3AppSupport/CommonRigidBodySetup.h"
+
+struct PhysicsSetup3DWorld : public CommonRigidBodySetup {
+	virtual void initPhysics(GraphicsPhysicsBridge& gfxBridge) {
+		createEmptyDynamicsWorld();
+	}
+};
 
 void test() {
-
-
+	PhysicsSetup3DWorld world;
 }
 
 
