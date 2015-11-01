f7cf43c8b42b4e6b08a09467e69e25e0e8ade448
Frank <Frank@5d77776e-c07b-11df-90e9-6755bb0d239d>
2015-11-01 06:41:13 +0000

    Experimenting with a wet rain effect on diffuse/specular lighting. Disabled for now, since it's not clear how to set this for wet vs. dry surfaces.

--- src/draw_world.cpp
+++ src/draw_world.cpp
@@ -384,6 +384,7 @@ void setup_smoke_shaders(shader_t &s, float min_alpha, int use_texgen, bool keep
 		s.add_uniform_int("burn_mask", 10);
 		select_multitex(DISINT_TEX, 10); // PLASMA_TEX?
 	}
+	//s.add_uniform_float("wet_effect", (is_rain_enabled() ? 1.0 : 0.0)); // FIXME: need to handle wet/outside vs. dry/inside surfaces differently
 }
 
 
