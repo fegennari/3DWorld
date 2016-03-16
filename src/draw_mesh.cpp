153af1b2155074cf7f1423bed4ad5c9896249319
Frank <Frank@5d77776e-c07b-11df-90e9-6755bb0d239d>
2016-03-16 06:17:01 +0000

    Allow skipping disabled mesh vertices on the first vertex, which is faster as it avoids alternating between drawn and skipped mesh vertices.

--- src/draw_mesh.cpp
+++ src/draw_mesh.cpp
@@ -337,10 +337,9 @@ public:
 	mesh_data_store() : c(0), reflection_pass(0) {last_rows.resize(MESH_X_SIZE+1);}
 
 	bool add_mesh_vertex_pair(int i, int j, float x, float y) {
-		if (c > 1) {
-			if (is_mesh_disabled(j, i) || is_mesh_disabled(j, i+1)) return 0;
-			if (mesh_z_cutoff > -FAR_DISTANCE && mesh_z_cutoff > max(mesh_height[i][j], mesh_height[i+1][j])) return 0;
-		}
+		if (is_mesh_disabled(j, i) || is_mesh_disabled(j, i+1)) return 0;
+		if (mesh_z_cutoff > -FAR_DISTANCE && mesh_z_cutoff > max(mesh_height[i][j], mesh_height[i+1][j])) return 0;
+		
 		for (unsigned p = 0; p < 2; ++p, ++c) {
 			int const iinc(min((MESH_Y_SIZE-1), int(i+p)));
 			//assert(c < data.size());
