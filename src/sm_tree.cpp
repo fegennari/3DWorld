5da0287cd9e174d283464a20eff24b030c1e8448
Frank <Frank@5d77776e-c07b-11df-90e9-6755bb0d239d>
2016-05-30 23:09:58 +0000

    Thicker pine tree trunks.

--- src/sm_tree.cpp
+++ src/sm_tree.cpp
@@ -42,12 +42,12 @@ struct sm_tree_type {
 };
 
 sm_tree_type const stt[NUM_ST_TYPES] = { // w2, ws, h, ss, c, tid
-	sm_tree_type(0.00, 0.10, 0.35, 0.4, PTREE_C, PINE_TEX,      BARK2_TEX), // T_PINE
+	sm_tree_type(0.00, 0.14, 0.35, 0.4, PTREE_C, PINE_TEX,      BARK2_TEX), // T_PINE
 	sm_tree_type(0.13, 0.15, 0.75, 0.8, TREE_C,  TREE_HEMI_TEX, BARK3_TEX), // T_DECID // HEDGE_TEX?
 	sm_tree_type(0.13, 0.15, 0.75, 0.7, TREE_C,  HEDGE_TEX,     BARK1_TEX), // T_TDECID
 	sm_tree_type(0.00, 0.15, 0.00, 0.8, TREE_C,  HEDGE_TEX,     BARK1_TEX), // T_BUSH NOTE: bark texture is not used in trees, but is used in logs
 	sm_tree_type(0.03, 0.15, 1.00, 0.6, TREE_C,  PALM_FROND_TEX,PALM_BARK_TEX), // T_PALM
-	sm_tree_type(0.00, 0.07, 0.00, 0.4, PTREE_C, PINE_TEX,      BARK2_TEX), // T_SH_PINE
+	sm_tree_type(0.00, 0.08, 0.00, 0.4, PTREE_C, PINE_TEX,      BARK2_TEX), // T_SH_PINE
 };
 
 bool is_pine_tree_type(int type) {return (type == T_PINE || type == T_SH_PINE);}
