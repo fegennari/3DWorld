cbb480a679981518cf3ec0c532ca6e019b895520
Frank <Frank@5d77776e-c07b-11df-90e9-6755bb0d239d>
2017-03-05 18:58:18 +0000

    Skip printing of notifications from debug callback

--- src/3DWorld.cpp
+++ src/3DWorld.cpp
@@ -2035,6 +2035,7 @@ void progress() {cout << "."; cout.flush();}
 
 void APIENTRY openglCallbackFunction(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, GLvoid* userParam) {
 
+	if (severity == GL_DEBUG_SEVERITY_NOTIFICATION) return; // don't spam stdout with notifications
 	cout << "---------------------opengl-callback-start------------" << endl;
 	cout << "message: "<< message << endl;
 	cout << "type: ";
@@ -2053,9 +2054,12 @@ void APIENTRY openglCallbackFunction(GLenum source, GLenum type, GLuint id, GLen
 	case GL_DEBUG_SEVERITY_LOW: cout << "LOW"; break;
 	case GL_DEBUG_SEVERITY_MEDIUM: cout << "MEDIUM"; break;
 	case GL_DEBUG_SEVERITY_HIGH: cout << "HIGH"; break;
+	case GL_DEBUG_SEVERITY_NOTIFICATION: cout << "NOTIFICATION"; break;
+	default: cout << hex << severity << dec << " ";
 	}
 	cout << endl;
 	cout << "---------------------opengl-callback-end--------------" << endl;
+	//assert(0);
 }
 
 void init_debug_callback() {
