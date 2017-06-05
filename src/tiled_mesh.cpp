9c8b31873536cfd365bd48cb1b2f7792f815f219
Frank <Frank@5d77776e-c07b-11df-90e9-6755bb0d239d>
2017-06-05 06:28:06 +0000

    Don't bind distant shadow maps for buildings

--- src/tiled_mesh.cpp
+++ src/tiled_mesh.cpp
@@ -1568,6 +1568,7 @@ void tile_t::bind_and_setup_shadow_map(shader_t &s) const {
 }
 bool tile_t::try_bind_shadow_map(shader_t &s) const {
 	if (!shadow_map_enabled() || smap_data.empty()) return 0;
+	if (get_dist_to_camera_in_tiles(1) > smap_thresh_scale) return 0; // too far to need smap, even if it exists
 	smap_data.set_for_all_lights(s, nullptr);
 	return 1;
 }
