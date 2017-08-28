d939a4a7a6c29bc5cb6a4fb47725cd9acbf85baf
Frank <Frank@5d77776e-c07b-11df-90e9-6755bb0d239d>
2017-08-28 02:36:52 +0000

    Removed the unused pool_allocator

--- src/allocators.h
+++ src/allocators.h
@@ -4,39 +4,6 @@
 #ifndef _ALLOCATORS_H_
 #define _ALLOCATORS_H_
 
-template <typename T, std::size_t pool_size = 1024>
-class pool_allocator {
-private:
-	std::vector<T*> d_pools;
-	T*              d_next;
-	T*              d_end;
-public:
-	template <typename O> struct rebind {typedef pool_allocator<O, pool_size> other;};
-	pool_allocator() : d_next(), d_end() {}
-	~pool_allocator() {
-		std::for_each(this->d_pools.rbegin(), this->d_pools.rend(), [](T* memory){ operator delete(memory); });
-	}
-	typedef T value_type;
-	T* allocate(std::size_t n) {
-		if (std::size_t(this->d_end - this->d_next) < n) {
-			if (pool_size < n) {
-				// custom allocation for bigger number of objects
-				this->d_pools.push_back(static_cast<T*>(operator new(sizeof(T) * n)));
-				return this->d_pools.back();
-			}
-			this->d_pools.push_back(static_cast<T*>(operator new(sizeof(T) * pool_size)));
-			this->d_next = this->d_pools.back();
-			this->d_end  = this->d_next + pool_size;
-		}
-		T* rc(this->d_next);
-		this->d_next += n;
-		return rc;
-	}
-	void deallocate(T*, std::size_t) {
-		// this could try to recycle buffers
-	}
-};
-
 template <typename T>
 class single_free_list_allocator {
 	vector<T *> free_list;
