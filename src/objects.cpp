2ac5042135f64f17a89d31bc0441ce5a73bd472e
Frank Gennari <fgennari@gmail.com>
2018-05-29 23:44:00 -0700

    Draw untextures transparent non attenuated sphere cobjs front-to-back

--- src/objects.cpp
+++ src/objects.cpp
@@ -571,7 +571,8 @@ void coll_obj::draw_cobj(unsigned &cix, int &last_tid, int &last_group_id, shade
 		// Note: if using textures (and using MVM in the shader), draw_sphere_vbo() is faster and can be used
 		// Note: semi-transparent spheres must not use tex coords because the translate in draw_sphere_vbo() produces the wrong indir lighting vpos;
 		// the transparent pass is sorted back-to-front and not grouped by texgen vs. tex-coord shader
-		if (use_tcs && !(is_semi_trans() && cp.tid < 0)) {draw_sphere_vbo(points[0], radius, ndiv, use_tcs);}
+		if (is_semi_trans() && cp.tid < 0 && !use_tcs && cp.light_atten == 0.0) {draw_sphere_vbo_back_to_front(points[0], radius, ndiv, use_tcs);}
+		else if (use_tcs && !(is_semi_trans() && cp.tid < 0)) {draw_sphere_vbo(points[0], radius, ndiv, use_tcs);}
 		else {draw_subdiv_sphere(points[0], radius, ndiv, use_tcs, 1);}
 		if (cp.light_atten > 0.0) {glDisable(GL_CULL_FACE);}
 		break;
