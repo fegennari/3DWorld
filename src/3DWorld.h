1ce184dccd3a9c26d9534bb7c7d19fdd8578c73f
Frank Gennari <fgennari@gmail.com>
2019-06-22 23:32:35 -0700

    Added pcg32_random_r() inline function to 3DWorld.h for future use (experimental)

--- src/3DWorld.h
+++ src/3DWorld.h
@@ -1463,6 +1463,20 @@ public:
 	double randd();
 };
 
+// *Really* minimal PCG32 code / (c) 2014 M.E. O'Neill / pcg-random.org
+// Licensed under Apache License 2.0 (NO WARRANTY, etc. see website)
+typedef struct { uint64_t state; uint64_t inc; } pcg32_random_t;
+
+inline uint32_t pcg32_random_r(pcg32_random_t* rng) {
+	uint64_t oldstate = rng->state;
+	// Advance internal state
+	rng->state = oldstate * 6364136223846793005ULL + (rng->inc|1);
+	// Calculate output function (XSH RR), uses old state for max ILP
+	uint32_t xorshifted = ((oldstate >> 18u) ^ oldstate) >> 27u;
+	uint32_t rot = oldstate >> 59u;
+	return (xorshifted >> rot) | (xorshifted << ((-(int32_t)rot) & 31));
+}
+
 class rgen_pregen_t : public rgen_core_t {
 
 	std::shared_ptr<vector<double>> pregen_rand_reals;
