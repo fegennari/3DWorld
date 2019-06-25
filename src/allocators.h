3ce1a669d8d9190de16b1ab311470d19504e57c0
Frank Gennari <fgennari@gmail.com>
2019-06-24 23:54:01 -0700

    Updated single_free_list_allocator so that it compiles with MSVS 2019

--- src/allocators.h
+++ src/allocators.h
@@ -9,16 +9,19 @@ class single_free_list_allocator {
 	vector<T *> free_list;
 public:
 	typedef T value_type;
+	typedef T* pointer;
+	typedef const T* const_pointer;
+
 	template <typename O> struct rebind {typedef single_free_list_allocator<O> other;};
 	void operator=(single_free_list_allocator const &a) {assert(free_list.empty() && a.free_list.empty());} // free list must be empty during copy
 	single_free_list_allocator(single_free_list_allocator const &a) {assert(a.free_list.empty());}
 	single_free_list_allocator() {}
 	~single_free_list_allocator() {
-		for (auto i = free_list.begin(); i != free_list.end(); ++i) {delete *i;}
+		for (auto i = free_list.begin(); i != free_list.end(); ++i) {free(*i);}
 	}
 	T* allocate(std::size_t n) {
 		assert(n == 1);
-		if (free_list.empty()) {return new T;}
+		if (free_list.empty()) {return static_cast<pointer>(malloc(sizeof(T)));}
 		T *ptr(free_list.back());
 		free_list.pop_back();
 		return ptr;
@@ -27,6 +30,9 @@ public:
 		assert(n == 1);
 		free_list.push_back(ptr);
 	}
+	void construct(pointer p, const T& val) {new(static_cast<void*>(p)) T(val);}
+	void construct(pointer p) {new(static_cast<void*>(p)) T();}
+	void destroy(pointer p) {p->~T();}
 };
 
 #endif // _ALLOCATORS_H_
