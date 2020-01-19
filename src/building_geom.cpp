5c83403e2a7e77754c793891e3c2222943a4d480
Frank Gennari <fgennari@gmail.com>
2020-01-19 13:18:07 -0800

    Fixes for player collision detection of exterior walls when inside a building

--- src/building_geom.cpp
+++ src/building_geom.cpp
@@ -159,18 +159,19 @@ bool building_t::check_sphere_coll(point &pos, point const &p_last, vector3d con
 			float cont_area(0.0);
 
 			for (auto p = parts.begin(); p != parts.end(); ++p) {
-				if (p->intersects_xy(sc)) {cont_area += (min(p->x2(), sc.x2()) - max(p->x1(), sc.x1()))*(min(p->y2(), sc.y2()) - max(p->y1(), sc.y1()));} // accumulate shared XY area
+				if (p->intersects(sc)) {cont_area += (min(p->x2(), sc.x2()) - max(p->x1(), sc.x1()))*(min(p->y2(), sc.y2()) - max(p->y1(), sc.y1()));} // accumulate shared XY area
 			}
 			if (cont_area < 0.99*sc.dx()*sc.dy()) { // sphere bounding cube not contained in union of parts - sphere is partially outside the building
 				c.expand_by_xy(-radius); // shrink part by sphere radius
 				c.clamp_pt_xy(pos2); // force pos2 into interior of the cube to prevent the sphere from intersecting the part
+				had_coll = 1;
 			}
 			is_interior = 1;
 			break; // flag for interior collision detection
 		} // for i
 	}
 	if (is_interior) {
-		had_coll = check_sphere_coll_interior(pos2, p_last, xlate, radius, xy_only, cnorm_ptr); // sphere collides with cube and check_interior=1
+		had_coll |= check_sphere_coll_interior(pos2, p_last, xlate, radius, xy_only, cnorm_ptr); // sphere collides with cube and check_interior=1
 	}
 	else {
 		for (auto i = parts.begin(); i != parts.end(); ++i) {
