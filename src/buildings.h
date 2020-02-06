9bfa3dd29a1117df420e489dfefe85acb9a7b503
Frank Gennari <fgennari@gmail.com>
2020-02-05 22:35:58 -0800

    Initialize other unused class member vars in building_t building_geom_t constructor

--- src/buildings.h
+++ src/buildings.h
@@ -301,8 +301,8 @@ struct building_t : public building_geom_t {
 	float ao_bcz2;
 
 	building_t(unsigned mat_ix_=0) : mat_ix(mat_ix_), hallway_dim(2), real_num_parts(0), is_house(0), has_antenna(0), has_chimney(0),
-		side_color(WHITE), roof_color(WHITE), detail_color(BLACK), ao_bcz2(0.0) {bcube.set_to_zeros();}
-	building_t(building_geom_t const &bg) : building_geom_t(bg) {}
+		side_color(WHITE), roof_color(WHITE), detail_color(BLACK), ao_bcz2(0.0) {}
+	building_t(building_geom_t const &bg) : building_geom_t(bg), mat_ix(0), hallway_dim(2), real_num_parts(0), is_house(0), has_antenna(0), has_chimney(0), ao_bcz2(0.0) {}
 	bool is_valid() const {return !bcube.is_all_zeros();}
 	bool has_interior() const {return bool(interior);}
 	bool has_room_geom() const {return (has_interior() && interior->room_geom);}
