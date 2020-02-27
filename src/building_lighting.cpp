82494e3f8fffba0a113329f932e48b7851a928cb
Frank Gennari <fgennari@gmail.com>
2020-02-26 22:43:26 -0800

    Only add upward pointing room light when it's above the camera

--- src/building_lighting.cpp
+++ src/building_lighting.cpp
@@ -383,7 +383,7 @@ void building_t::add_room_lights(vector3d const &xlate, unsigned building_id, bo
 		dl_sources.emplace_back(light_radius, lpos, lpos, color, 0, -plus_z, bwidth); // points down, white for now
 		dl_sources.back().set_custom_bcube(clipped_bc);
 
-		if (camera_near_building) { // only when the player is near/inside a building and can't see the light bleeding through the floor
+		if (camera_near_building && lpos.z > camera_bs.z) { // only when the player is near/inside a building and can't see the light bleeding through the floor
 			// add a smaller unshadowed light with 360 deg FOV to illuminate the ceiling and other areas as cheap indirect lighting
 			point const lpos_up(lpos - vector3d(0.0, 0.0, 2.0*i->dz()));
 			dl_sources.emplace_back(0.5*((room.is_hallway ? 0.3 : room.is_office ? 0.3 : 0.5))*light_radius, lpos_up, lpos_up, color);
