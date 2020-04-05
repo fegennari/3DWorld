f95e1c6149ddb9de8d54234214909fae8ba8542f
Frank Gennari <fgennari@gmail.com>
2020-04-04 23:05:25 -0700

    Fixed direction instability of people AI

--- src/building_navigation.cpp
+++ src/building_navigation.cpp
@@ -548,7 +548,7 @@ int building_t::ai_room_update(building_ai_state_t &state, rand_gen_t &rgen, vec
 	}
 	float const max_dist(person.speed*fticks);
 
-	if (dist_less_than(person.pos, person.target_pos, max_dist)) { // at dest
+	if (dist_less_than(person.pos, person.target_pos, 1.1f*max_dist)) { // at dest
 		if (!stay_on_one_floor && person.pos.z != person.target_pos.z) {} // handle this case
 		assert(bcube.contains_pt(person.target_pos));
 		person.pos = person.target_pos;
@@ -567,11 +567,12 @@ int building_t::ai_room_update(building_ai_state_t &state, rand_gen_t &rgen, vec
 	point new_pos;
 	
 	if (dot_product(new_dir, person.dir) < 0.999*new_dir_mag) { // dir not perfectly aligned
-		//if (person.is_close_to_player()) {cout << TXT(new_dir.str()) << TXT(person.dir.str()) << TXT(new_dir_mag) << TXT(delta_dir) << TXT(max_dist) << endl;}
+		//if (person.is_close_to_player()) {cout << TXT(new_dir.str()) << TXT(person.dir.str()) << TXT(new_dir_mag) << TXT(delta_dir) << TXT(max_dist) << TXT(person.radius) << endl;}
 		assert(new_dir_mag > TOLERANCE); // should be guaranteed by dist_less_than() test, assuming zvals are equal (which they should be)
+		float const step_scale(max(0.1f, dot_product(person.dir, new_dir)/new_dir_mag)); // move more slowly when direction misaligns to avoid overshooting target_pos
 		person.dir = (delta_dir/new_dir_mag)*new_dir + (1.0 - delta_dir)*person.dir; // merge new_dir into dir gradually for smooth turning
 		person.dir.normalize();
-		new_pos = person.pos + max_dist*person.dir;
+		new_pos = person.pos + (max_dist*step_scale)*person.dir;
 		cube_t clip_cube(bcube);
 		clip_cube.expand_by_xy(-coll_dist); // shrink
 		clip_cube.clamp_pt_xy(new_pos); // make sure person stays within building bcube; can't clip to room because person may be exiting it
