1bdfa15eacfbc36496b75b9b0e451cb38c78c358
Frank Gennari <fgennari@gmail.com>
2020-05-10 23:58:28 -0700

    Use a slightly larger light radius for unshadowed room lights on the top floor of the building

--- src/building_lighting.cpp
+++ src/building_lighting.cpp
@@ -528,8 +528,11 @@ void building_t::add_room_lights(vector3d const &xlate, unsigned building_id, bo
 
 		if (camera_near_building && lpos.z > camera_bs.z) { // only when the player is near/inside a building and can't see the light bleeding through the floor
 			// add a smaller unshadowed light with 360 deg FOV to illuminate the ceiling and other areas as cheap indirect lighting
+			// since we're not enabling shadows for this light, it could incorrectly illuminate objects on the floor above, so we must make it small, unless it's on the top floor
+			bool const at_top(lpos.z > room.z2() - 0.5f*get_window_vspace());
+			float const radius_scale(at_top ? 0.55 : 0.5);
 			point const lpos_up(lpos - vector3d(0.0, 0.0, 2.0*i->dz()));
-			dl_sources.emplace_back(0.5*((room.is_hallway ? 0.3 : room.is_office ? 0.3 : 0.5))*light_radius, lpos_up, lpos_up, color);
+			dl_sources.emplace_back(radius_scale*((room.is_hallway ? 0.3 : room.is_office ? 0.3 : 0.5))*light_radius, lpos_up, lpos_up, color);
 			dl_sources.back().set_custom_bcube(clipped_bc); // Note: could reduce clipped_bc further if needed
 			dl_sources.back().disable_shadows();
 		}
