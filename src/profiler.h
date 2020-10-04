c0087ea1c18fcfe311c3493ae15a11cb4aa42ba4
Frank Gennari <fgennari@gmail.com>
2020-10-03 22:31:48 -0700

    Reordered member variable initialization for highres_timer_t

--- src/profiler.h
+++ src/profiler.h
@@ -14,8 +14,8 @@ class highres_timer_t {
 	high_resolution_clock::time_point timer1;
 	high_resolution_clock clock;
 public:
-	highres_timer_t(char const *const name_,  bool enabled_=1) : name(name_), timer1(clock.now()), enabled(enabled_) {}
-	highres_timer_t(std::string const &name_, bool enabled_=1) : name(name_), timer1(clock.now()), enabled(enabled_) {}
+	highres_timer_t(char const *const name_,  bool enabled_=1) : name(name_), enabled(enabled_), timer1(clock.now()) {}
+	highres_timer_t(std::string const &name_, bool enabled_=1) : name(name_), enabled(enabled_), timer1(clock.now()) {}
 	~highres_timer_t() {end();}
 	void end();
 };
