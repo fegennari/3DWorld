f8980a3f9c71be6be163452fa28ee0a9c418b6fe
Frank Gennari <fgennari@gmail.com>
2020-10-16 00:02:36 -0700

    Added lamp model for building rooms, not yet placed

--- src/city_building_params.cpp
+++ src/city_building_params.cpp
@@ -172,6 +172,9 @@ bool city_params_t::read_option(FILE *fp) {
 	else if (str == "office_chair_model") {
 		if (!add_model(OBJ_MODEL_OFFICE_CHAIR, fp)) {return read_error(str);}
 	}
+	else if (str == "lamp_model") {
+		if (!add_model(OBJ_MODEL_LAMP, fp)) {return read_error(str);}
+	}
 	else {
 		cout << "Unrecognized city keyword in input file: " << str << endl;
 		return 0;
