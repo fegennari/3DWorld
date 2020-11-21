134a6b660fef76b35116a01e9ccea478cf32e80d
Frank Gennari <fgennari@gmail.com>
2020-11-20 22:29:23 -0800

    Fix for plants addition affecting building random number generator state

--- src/building_rooms.cpp
+++ src/building_rooms.cpp
@@ -1406,7 +1406,7 @@ bool building_t::hang_pictures_in_room(rand_gen_t rgen, room_t const &room, floa
 	return was_hung;
 }
 
-void building_t::add_plants_to_room(rand_gen_t &rgen, room_t const &room, float zval, unsigned room_id, float tot_light_amt, unsigned objs_start, unsigned num) {
+void building_t::add_plants_to_room(rand_gen_t rgen, room_t const &room, float zval, unsigned room_id, float tot_light_amt, unsigned objs_start, unsigned num) {
 	float const window_vspacing(get_window_vspace());
 	cube_t place_area(get_walkable_room_bounds(room));
 	place_area.expand_by(-0.1*get_wall_thickness()); // shrink to leave a small gap
