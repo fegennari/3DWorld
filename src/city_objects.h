017afce36283d2aa0a191bfdc055092cd88b9648
Frank Gennari <fgennari@gmail.com>
2021-08-15 00:12:14 -0700

    Split cityobj_placer_t class into new files city_objects.{cpp,h}

---
+++ src/city_objects.h
@@ -0,0 +1,92 @@
+// 3D World - City Object Placement Header
+// by Frank Gennari
+// 08/14/21
+
+#pragma once
+
+#include "city.h"
+
+struct city_obj_t : public sphere_t {
+	cube_t bcube;
+	city_obj_t() {}
+	city_obj_t(point const &pos_, float radius_) : sphere_t(pos_, radius_) {}
+	bool operator<(city_obj_t const &b) const {return (bcube.x1() < b.bcube.x1());} // sort by bcube x1
+	static void post_draw(draw_state_t &dstate, bool shadow_only) {}
+};
+
+struct bench_t : public city_obj_t {
+	bool dim, dir;
+	bench_t() : dim(0), dir(0) {}
+	void calc_bcube();
+	static void pre_draw(draw_state_t &dstate, bool shadow_only);
+	void draw(draw_state_t &dstate, quad_batch_draw &qbd, float dist_scale, bool shadow_only) const;
+	bool proc_sphere_coll(point &pos_, point const &p_last, float radius_, point const &xlate, vector3d *cnorm) const;
+};
+
+struct tree_planter_t : public city_obj_t {
+	tree_planter_t(point const &pos_, float radius_, float height);
+	static void pre_draw(draw_state_t &dstate, bool shadow_only);
+	void draw(draw_state_t &dstate, quad_batch_draw &qbd, float dist_scale, bool shadow_only) const;
+};
+
+struct fire_hydrant_t : public city_obj_t {
+	float cylin_radius;
+	vector3d orient;
+	fire_hydrant_t(point const &pos_, float radius_, float height, vector3d const &orient_);
+	static void pre_draw(draw_state_t &dstate, bool shadow_only);
+	static void post_draw(draw_state_t &dstate, bool shadow_only);
+	void draw(draw_state_t &dstate, quad_batch_draw &qbd, float dist_scale, bool shadow_only) const;
+	bool proc_sphere_coll(point &pos_, point const &p_last, float radius_, point const &xlate, vector3d *cnorm) const;
+};
+
+class city_obj_placer_t {
+public: // road network needs access to parking lots and driveways for drawing
+	vector<parking_lot_t> parking_lots;
+	vector<driveway_t> driveways; // for houses
+private:
+	struct plot_divider_type_t {
+		string tex_name;
+		float wscale, hscale; // width and height scales
+		plot_divider_type_t(string const &tn, float ws, float hs) : tex_name(tn), wscale(ws), hscale(hs) {}
+	};
+	enum {DIV_WALL=0, DIV_FENCE, DIV_HEDGE, DIV_NUM_TYPES}; // types of plot dividers, with end terminator
+	plot_divider_type_t plot_divider_types[DIV_NUM_TYPES] = {
+		plot_divider_type_t("cblock2.jpg", 0.5, 2.5),  // wall
+		plot_divider_type_t("fence.jpg",   0.1, 2.0),  // fence
+		plot_divider_type_t("hedges.jpg",  1.0, 1.6)}; // hedge
+	vector<bench_t> benches;
+	vector<tree_planter_t> planters;
+	vector<fire_hydrant_t> fire_hydrants;
+	vector<cube_with_ix_t> dividers; // dividers for residential plots; index is type
+	vector<cube_with_ix_t> bench_groups, planter_groups, fire_hydrant_groups, divider_groups; // index is last object in group
+	quad_batch_draw qbd;
+	unsigned num_spaces, filled_spaces;
+	
+	struct cube_by_x1 {
+		bool operator()(cube_t const &a, cube_t const &b) const {return (a.x1() < b.x1());}
+	};
+	bool gen_parking_lots_for_plot(cube_t plot, vector<car_t> &cars, unsigned city_id, unsigned plot_ix, vect_cube_t &bcubes, vect_cube_t &colliders, rand_gen_t &rgen);
+	void add_cars_to_driveways(vector<car_t> &cars, vector<road_plot_t> const &plots, vector<vect_cube_t> &plot_colliders, unsigned city_id, rand_gen_t &rgen) const;
+	static bool check_pt_and_place_blocker(point const &pos, vect_cube_t &blockers, float radius, float blocker_spacing);
+	static bool try_place_obj(cube_t const &plot, vect_cube_t &blockers, rand_gen_t &rgen, float radius, float extra_spacing, unsigned num_tries, point &pos);
+	static void place_tree(point const &pos, float radius, int ttype, vect_cube_t &colliders, vector<point> &tree_pos, bool allow_bush, bool is_sm_tree);
+	static void place_trees_in_plot(road_plot_t const &plot, vect_cube_t &blockers, vect_cube_t &colliders, vector<point> &tree_pos, rand_gen_t &rgen);
+	template<typename T> void add_obj_to_group(T const &obj, cube_t const &bcube, vector<T> &objs, vector<cube_with_ix_t> &groups, bool &is_new_tile);
+	template<typename T> void sort_grouped_objects(vector<T> &objs, vector<cube_with_ix_t> const &groups);
+	void place_detail_objects(road_plot_t const &plot, vect_cube_t &blockers, vect_cube_t &colliders, vector<point> const &tree_pos,
+		rand_gen_t &rgen, bool is_new_tile, bool is_residential, float plot_subdiv_sz);
+	void add_house_driveways(road_plot_t const &plot, vect_cube_t &temp_cubes, rand_gen_t &rgen, unsigned plot_ix);
+	template<typename T> void draw_objects(vector<T> const &objs, vector<cube_with_ix_t> const &groups,
+		draw_state_t &dstate, float dist_scale, bool shadow_only, bool not_using_qbd=0);
+public:
+	void clear();
+	void gen_parking_and_place_objects(vector<road_plot_t> &plots, vector<vect_cube_t> &plot_colliders, vector<car_t> &cars,
+		unsigned city_id, bool have_cars, bool is_residential, float plot_subdiv_sz);
+	static bool subdivide_plot_for_residential(cube_t const &plot, float plot_subdiv_sz, vect_city_zone_t &sub_plots);
+	void draw_detail_objects(draw_state_t &dstate, bool shadow_only);
+	bool proc_sphere_coll(point &pos, point const &p_last, float radius, vector3d *cnorm) const;
+	bool line_intersect(point const &p1, point const &p2, float &t) const;
+	bool get_color_at_xy(point const &pos, colorRGBA &color) const;
+};
+
+inline uint64_t get_tile_id_for_cube(cube_t const &c) {return get_tile_id_containing_point_no_xyoff(c.get_cube_center());}
