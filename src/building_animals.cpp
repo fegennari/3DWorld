a219badfe54d46c7e1e3d635c628533d9a3f3d28
Frank Gennari <fgennari@gmail.com>
2022-01-19 22:50:51 -0800

    Working on building rat movement/collision logic

--- src/building_animals.cpp
+++ src/building_animals.cpp
@@ -10,6 +10,7 @@ float const RAT_FOV_DEG     = 60.0; // field of view in degrees
 float const RAT_VIEW_FLOORS = 4.0; // view distance in floors
 float const RAT_FOV_DP(cos(0.5*RAT_FOV_DEG*TO_RADIANS));
 
+extern int animate2;
 extern float fticks;
 extern building_params_t global_building_params;
 extern object_model_loader_t building_obj_model_loader;
@@ -41,7 +42,8 @@ cube_t rat_t::get_bcube_with_dir() const {
 }
 
 void building_t::update_animals(unsigned building_ix) {
-	if (is_rotated() || !has_room_geom() || interior->rooms.empty() || global_building_params.num_rats_max == 0) return;
+	if (global_building_params.num_rats_max == 0 || !animate2) return;
+	if (is_rotated() || !has_room_geom() || interior->rooms.empty()) return;
 	vect_rat_t &rats(interior->room_geom->rats);
 	if (rats.placed && rats.empty()) return; // no rats placed in this building
 	if (!building_obj_model_loader.is_model_valid(OBJ_MODEL_RAT)) return; // no rat model
@@ -90,12 +92,15 @@ template<typename T> bool line_int_cubes_exp(point const &p1, point const &p2, v
 	}
 	return 0;
 }
-bool can_hide_under(room_object type) {
-	return (type == TYPE_BED || type == TYPE_DESK || type == TYPE_DRESSER || type == TYPE_NIGHTSTAND || type == TYPE_TABLE || type == TYPE_CHAIR);
+bool can_hide_under(room_object_t const &c) {
+	if (c.shape == SHAPE_CYLIN) return 0; // skip round tables for now
+	if (c.shape != SHAPE_CUBE ) return 0; // cubes only for now
+	return (c.type == TYPE_BED || c.type == TYPE_DESK || c.type == TYPE_DRESSER || c.type == TYPE_NIGHTSTAND || c.type == TYPE_TABLE || c.type == TYPE_CHAIR);
 }
 
 // p1 and p2 are line end points; radius applies in X and Y, hheight is half height and applies in +/- z
 bool building_t::check_line_of_sight(point const &p1, point const &p2, float radius, float hheight) const {
+	assert(p1.z == p2.z); // assumes same zval; can relax this later if needed
 	assert(interior != nullptr);
 	vector3d const expand(radius, radius, hheight);
 	
@@ -104,7 +109,10 @@ bool building_t::check_line_of_sight(point const &p1, point const &p2, float rad
 		if (line_int_cubes_exp(p1, p2, interior->walls[d], expand)) return 0;
 	}
 	for (auto const &door : interior->doors) {
-		if (!door.open && line_int_cube_exp(p1, p2, door, expand)) return 0; // what about open doors? (see code for ball collision)
+		if (door.open) {
+			// TODO: see building_interior_t::check_sphere_coll_walls_elevators_doors()
+		}
+		else if (line_int_cube_exp(p1, p2, door, expand)) return 0; // what about open doors? (see code for ball collision)
 	}
 	if (line_int_cubes_exp(p1, p2, interior->stairwells, expand)) return 0;
 	if (line_int_cubes_exp(p1, p2, interior->elevators,  expand)) return 0;
@@ -115,15 +123,20 @@ bool building_t::check_line_of_sight(point const &p1, point const &p2, float rad
 	if (!has_room_geom()) return 1; // done (but really shouldn't get here)
 	// check room objects; ignore expanded objects for now
 	auto objs_end(interior->room_geom->get_std_objs_end()); // skip buttons/stairs/elevators
-	float const obj_z2(p1.z + hheight);
+	float const obj_z1(p1.z - hheight), obj_z2(p1.z + hheight);
 
 	for (auto c = interior->room_geom->objs.begin(); c != objs_end; ++c) {
-		if (c->no_coll() || !bldg_obj_types[c->type].ai_coll) continue; // skip non-colliding objects
+		if (c->no_coll() || !bldg_obj_types[c->type].ai_coll) continue; // skip non-colliding objects; why does trashcan not work?
+		if (c->z1() > obj_z2 || c->z2() < obj_z1)   continue; // wrong floor
+		if (!line_int_cube_exp(p1, p2, *c, expand)) continue;
 		
-		if (can_hide_under(c->type)) { // check for objects we can hide under
+		if (can_hide_under(*c)) { // check for objects we can hide under
 			// TODO: detailed collision with only object legs, and maybe bottom if obj_z2 is low enough
 		}
-		if (line_int_cube_exp(p1, p2, *c, expand)) return 0;
+		else if (c->type == TYPE_CLOSET ) {} // TODO
+		else if (c->type == TYPE_STALL  ) {} // TODO
+		else if (c->type == TYPE_CUBICLE) {} // TODO
+		return 0;
 	} // for c
 	return 1;
 }
@@ -132,46 +145,51 @@ void building_t::update_rat(rat_t &rat, rand_gen_t &rgen) const {
 	float const floor_spacing(get_window_vspace()), view_dist(RAT_VIEW_FLOORS*floor_spacing);
 	float const length(rat.get_length()), width(rat.get_width()), height(rat.get_height()), hheight(0.5*height);
 	float const coll_radius(1.2*width); // maybe should use length so that the rat doesn't collide when turning?
+	float const line_project_dist(0.5*1.1*(length - width)); // extra space in front of the target destination
 	float const move_dist(fticks*rat.speed);
 
 	if (rat.speed > 0.0) {
 		// TODO: collision detection with dynamic objects: doors, balls, the player? people? other animals?
 		rat.pos += move_dist*rat.dir; // apply movement
 	}
-	if (rat.fear > 0.0) {
-		// find hiding spot (in opposite direction from fear_pos?)
+	if (rat.fear > 0.0) { // find hiding spot (in opposite direction from fear_pos?)
 		auto objs_end(interior->room_geom->get_std_objs_end()); // skip buttons/stairs/elevators
 
 		for (auto c = interior->room_geom->objs.begin(); c != objs_end; ++c) {
-			if (!can_hide_under(c->type)) continue;
+			if (!can_hide_under(*c)) continue;
 			if (c->z1() > (rat.pos.z + height) || c->z2() < rat.pos.z) continue; // wrong floor
-			// TODO: consider this item
+			// TODO: consider this object as a hiding spot
 		} // for c
 		rat.fear = max(0.0f, (rat.fear - 0.1f*(fticks/TICKS_PER_SECOND))); // reduce fear over 10s
 	}
 	else if (dist_less_than(rat.pos, rat.dest, max(0.05f*rat.radius, 1.5f*move_dist))) { // no dest/at dest - choose a new dest
 		vector3d const center_dz(0.0, 0.0, hheight);
 		rat.speed = 0.0; // stop until we've found a valid destination
+		float target_fov_dp(RAT_FOV_DP), target_max_dist(view_dist); // start at nominal/max values
 
-		for (unsigned n = 0; n < 100; ++n) { // make 100 tries
+		for (unsigned n = 0; n < 200; ++n) { // make 100 tries
+			if (n > 50) { // we've been at this for a while, maybe we need to relax our constraints, maybe follow the walls?
+				target_fov_dp   -= 0.02; // allow for turns outside our field of view
+				target_max_dist *= 0.96;  // decrease the max distance considered
+			}
 			vector3d const vdir(rgen.signed_rand_vector_xy().get_norm()); // random XY direction
-			if (dot_product(rat.dir, vdir) < RAT_FOV_DP) continue; // not in field of view, use a new direction
-			float const dist(rgen.rand_uniform(0.1, 1.0)*view_dist); // random distance out to max view dist
+			if (dot_product(rat.dir, vdir) < target_fov_dp) continue; // not in field of view, use a new direction
+			float const dist(rgen.rand_uniform(0.1, 1.0)*target_max_dist); // random distance out to max view dist
 			point const cand(rat.pos + dist*vdir);
-			point const p1(rat.pos + center_dz), p2(cand + 0.5f*(length - width)*vdir + center_dz); // extend in vdir so that the head doesn't collide
+			if (!bcube.contains_pt_xy_exp(cand, -coll_radius)) continue; // check for end point inside building bcube
+			point const p1(rat.pos + center_dz), p2(cand + line_project_dist*vdir + center_dz); // extend in vdir so that the head doesn't collide
 			if (!check_line_of_sight(p1, p2, coll_radius, hheight)) continue;
 			rat.dest  = cand;
 			rat.speed = global_building_params.rat_speed*rgen.rand_uniform(0.5, 1.0); // random speed
 			break; // success
 		} // for n
 	}
-	if (rat.dest != rat.pos) {
-		rat.dir = (rat.dest - rat.pos).get_norm(); // TODO: slow turn (like people)
-	}
+	if (rat.dest != rat.pos) {rat.dir = (rat.dest - rat.pos).get_norm();} // TODO: slow turn (like people)
 	assert(rat.dir.z == 0.0); // must be in XY plane
 }
 
 void building_t::scare_animals(point const &scare_pos, float sight_amt, float sound_amt) {
+	if (!animate2) return; // don't accumulate fear when AI is not updating
 	vector<rat_t> &rats(interior->room_geom->rats);
 	if (rats.empty()) return; // nothing to do
 	float const amount(min((sight_amt + sound_amt), 1.0f)); // for now we don't differentiate between sight and sound for rats
