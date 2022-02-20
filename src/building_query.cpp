f4df4165b957d8c3b7f91ce28d7d20bee2dbdce6
Frank Gennari <fgennari@gmail.com>
2022-02-19 18:42:35 -0800

    building_t::overlaps_other_room_obj() must include blockers; Added comments explaining this

--- src/building_query.cpp
+++ src/building_query.cpp
@@ -902,7 +902,8 @@ bool building_t::check_for_wall_ceil_floor_int(point const &p1, point const &p2)
 	return check_line_intersect_doors(p1, p2);
 }
 
-bool building_t::overlaps_other_room_obj(cube_t const &c, unsigned objs_start, bool check_all) const { // Note: no expanded_objs
+// Note: for procedural object placement; no expanded_objs, but includes blockers
+bool building_t::overlaps_other_room_obj(cube_t const &c, unsigned objs_start, bool check_all) const {
 	assert(has_room_geom());
 	vect_room_object_t &objs(interior->room_geom->objs);
 	assert(objs_start <= objs.size());
@@ -910,10 +911,11 @@ bool building_t::overlaps_other_room_obj(cube_t const &c, unsigned objs_start, b
 	for (auto i = objs.begin()+objs_start; i != objs.end(); ++i) {
 		// Note: light switches don't collide with the player or AI, but they collide with other placed objects to avoid blocking them;
 		// however, it's okay to block outlets with furniture
-		if ((check_all || !i->no_coll() || i->type == TYPE_SWITCH) && i->type != TYPE_BLOCKER && i->intersects(c)) return 1;
+		if ((check_all || !i->no_coll() || i->type == TYPE_SWITCH) && i->intersects(c)) return 1;
 	}
 	return 0;
 }
+// Note: for dynamic player object placement; includes expanded_objs but not blockers
 bool building_t::overlaps_any_placed_obj(cube_t const &c) const { // Note: includes expanded_objs
 	assert(has_room_geom());
 	auto objs_end(interior->room_geom->get_std_objs_end()); // skip buttons; must include stairs and elevators
