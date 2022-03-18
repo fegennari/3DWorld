750614ba20f33bce52d5fb37afdc26f1f2c2acae
Frank Gennari <fgennari@gmail.com>
2022-03-18 10:27:41 -0700

    Fix for alignment of bottom floor U-shaped stairs back railing; still slightly off, but maybe close enough

--- src/building_rooms.cpp
+++ src/building_rooms.cpp
@@ -3227,11 +3227,11 @@ void building_t::add_stairs_and_elevators(rand_gen_t &rgen) {
 		// add walls and railings
 		bool const extend_walls_up(i->is_at_top && !i->roof_access); // space above is open, add a wall so that people can't fall down the stairs
 		float const railing_z2(i->z2() + (i->roof_access ? 0.025*i->dz() : 0.0)); // capture z2 before we change it; move roof access railing up a bit to offset the shrink resize
-		float const railing_side_dz(0.5*stair_dz); // for U-shaped stairs
+		float const wall_bottom(floor_z - half_thick), railing_side_dz(0.5*stair_dz); // for U-shaped stairs
 		cube_t wall(*i);
 		if (extend_walls_up) {wall.z2() += window_vspacing - floor_thickness;}
 		else {wall.z2() -= 0.5*floor_thickness;} // prevent z-fighting on top floor
-		wall.z1() = max(bcube.z1()+half_thick, floor_z-half_thick); // full height
+		wall.z1() = max(bcube.z1()+half_thick, wall_bottom); // full height
 		set_wall_width(wall, i->d[dim][dir], wall_hw, dim);
 
 		if ((i->shape == SHAPE_WALLED && !(i->against_wall[0] || i->against_wall[1]) && (!i->stack_conn || !i->is_at_top)) || i->shape == SHAPE_U) {
@@ -3277,10 +3277,10 @@ void building_t::add_stairs_and_elevators(rand_gen_t &rgen) {
 			}
 		} // for d
 		if (i->has_railing && i->shape == SHAPE_U) { // add a railing for the back wall of U-shaped stairs
-			float const railing_dz(((wall.z1() < ground_floor_z1) ? 0.77 : 0.819)*window_vspacing); // determined experimentally, and different for basement stacked part conn stairs
+			float const railing_zc(wall_bottom + 0.819*window_vspacing); // determined experimentally
 			cube_t railing(*i);
 			set_wall_width(railing, (i->d[dim][dir] + (dir ? -1.0 : 1.0)*2.0*wall_hw), wall_hw, dim);
-			set_wall_width(railing, (wall.z1() + railing_dz), 1.4*railing_side_dz, 2); // set zvals
+			set_wall_width(railing, railing_zc, 1.4*railing_side_dz, 2); // set zvals
 			objs.emplace_back(railing, TYPE_RAILING, 0, !dim, dir, (RO_FLAG_NOCOLL | RO_FLAG_ADJ_HI | RO_FLAG_ADJ_LO | RO_FLAG_ADJ_BOT), 1.0, SHAPE_CUBE, railing_color); // no ends
 		}
 		else if (i->has_railing && (i->stack_conn || (extend_walls_up && i->shape == SHAPE_STRAIGHT))) {
