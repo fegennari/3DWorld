f34af543017405b04be17376966fec494564f487
Frank Gennari <fgennari@gmail.com>
2022-03-31 21:09:06 -0700

    Added a few code comments

--- src/Math3d.cpp
+++ src/Math3d.cpp
@@ -998,7 +998,7 @@ bool sphere_cube_intersect(point const &pos, float radius, cube_t const &cube, p
 		for (unsigned i = 0; i < unsigned(2 + !skip_z); ++i) {
 			for (unsigned j = 0; j < 2; ++j) {
 				//if (iter == 0 && pos[i] != p_last[i] && ((pos[i] - p_last[i]) < 0) ^ j) continue; // ignore back-facing sides (is this correct?)
-				float const delta(j ? 1.0 : -1.0), side_pos(cube.d[i][j] + delta*radius);
+				float const delta(j ? 1.0 : -1.0), side_pos(cube.d[i][j] + delta*radius); // cube expanded by radius
 				if (iter == 0 && !((p_last[i] < side_pos) ^ j) && ((pos[i] >= side_pos) ^ j)) continue;
 				float const dist(fabs(pos[i] - side_pos));
 
@@ -1011,9 +1011,9 @@ bool sphere_cube_intersect(point const &pos, float radius, cube_t const &cube, p
 					cdir     = (i << 1) + j; // x0 x1 y0 y1 z0 z1
 					found    = 1;
 				}
-			}
-		}
-	}
+			} // for j
+		} // for i
+	} // for iter
 	return found;
 }
 
