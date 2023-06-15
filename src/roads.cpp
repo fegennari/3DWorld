4db5122fdded7c14e953c0be5c488f3a59f0f08f
Frank Gennari <fgennari@gmail.com>
2023-06-15 00:49:13 -0700

    Increased the height of street signs above stop signs so that trucks don't hit them

--- src/roads.cpp
+++ src/roads.cpp
@@ -765,7 +765,7 @@ void road_isec_t::draw_stoplights_and_street_signs(road_draw_state_t &dstate, ve
 			spos[dim] += (dir ? 1.0 : -1.0)*0.07*ss_height; // move a bit behind the stop sign
 			sc.set_from_point(spos);
 			sc.expand_by_xy(0.02*ss_height);
-			set_cube_zvals(sc, z1(), (z1() + 1.4*ss_height));
+			set_cube_zvals(sc, z1(), (z1() + 2.0*ss_height));
 			if (dist_val < 0.04) {dstate.draw_cube(dstate.qbd_untextured, sc, GRAY);} // draw the sign pole if close
 		}
 		if (draw_street_sign) { // draw street sign
