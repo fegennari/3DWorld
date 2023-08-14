2ce904d8ad60d7edf3aaac9884369cc0937316e6
Frank Gennari <fgennari@gmail.com>
2023-08-13 21:21:53 -0700

    Only place backrooms light switches next to the main entrance door

--- src/building_room_obj_place.cpp
+++ src/building_room_obj_place.cpp
@@ -2551,6 +2551,7 @@ void building_t::add_light_switches_to_room(rand_gen_t rgen, room_t const &room,
 		unsigned num_ls(0);
 
 		for (auto i = cands.begin(); i != cands.end() && num_ls < max_ls; ++i) {
+			if (!is_house && room.is_ext_basement() && room_bounds.contains_cube_xy(*i)) continue; // skip interior backrooms doors
 			// check for windows if (real_num_parts > 1)? is it actually possible for doors to be within far_spacing of a window?
 			bool const dim(i->dim), dir(i->get_center_dim(dim) > room.get_center_dim(dim));
 			float const dir_sign(dir ? -1.0 : 1.0), door_width(i->get_width()), near_spacing(0.25*door_width), far_spacing(1.25*door_width); // off to side of door when open
