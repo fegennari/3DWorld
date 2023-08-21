712c70e5535e1be374240f29bcd670a38a13c4d8
Frank Gennari <fgennari@gmail.com>
2023-08-20 21:04:45 -0700

    Fix for zombies unfairly damaging the player while on stairs

--- src/building_navigation.cpp
+++ src/building_navigation.cpp
@@ -1912,10 +1912,11 @@ int building_t::ai_room_update(person_t &person, float delta_dir, unsigned perso
 	build_nav_graph();
 
 	if (can_ai_follow_player(person, allow_diff_building)) {
-		// use zval of the feet to handle cases where the person and the player are different heights
-		point const feet_pos(person.pos.x, person.pos.y, person.get_z1()), player_feet_pos(cur_player_building_loc.pos - vector3d(0.0, 0.0, get_bldg_player_height()));
+		// check for player intersection/damage; use zval of the feet to handle cases where the person and the player are different heights
+		float const player_height(get_bldg_player_height());
+		point const feet_pos(person.pos.x, person.pos.y, person.get_z1()), player_feet_pos(cur_player_building_loc.pos - vector3d(0.0, 0.0, player_height));
 
-		if (dist_less_than(feet_pos, player_feet_pos, 1.2f*(person.radius + get_scaled_player_radius()))) { // intersecting the player
+		if (fabs(feet_pos.z - player_feet_pos.z) < 0.5*player_height && dist_less_than(feet_pos, player_feet_pos, 1.2f*(person.radius + get_scaled_player_radius()))) {
 			if (!check_for_wall_ceil_floor_int(person.pos, cur_player_building_loc.pos, 1)) { // inc_pg_br_walls=1
 				int const ret_status(register_ai_player_coll(person.has_key, person.get_height())); // return value: 0=no effect, 1=player is killed, 2=this person is killed
 				// player is killed, we could track kills here
