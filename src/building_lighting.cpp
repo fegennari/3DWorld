42138736bc45048edafea3d4e5ebc7014981e515
Frank Gennari <fgennari@gmail.com>
2023-08-22 23:45:52 -0700

    Culling of lights when either the light or camera is in the lower level and the other is not in the extended basement

--- src/building_lighting.cpp
+++ src/building_lighting.cpp
@@ -1328,6 +1328,8 @@ void building_t::add_room_lights(vector3d const &xlate, unsigned building_id, bo
 		float const floor_below_zval(floor_z - window_vspacing), ceil_above_zval(ceil_z + window_vspacing);
 		// Note: we use level_z rather than floor_z for floor_is_above test so that it agrees with the threshold logic for player_in_basement
 		bool const floor_is_above((camera_z < level_z) && !is_single_floor), floor_is_below(camera_z > ceil_z);
+		if (!is_house && floor_is_below && in_ext_basement && !camera_in_ext_basement && lpos.z   < get_basement().z1()) continue; // light in lower level extb, player not in extb
+		if (!is_house && floor_is_above && !in_ext_basement && camera_in_ext_basement && camera_z < get_basement().z1()) continue; // player in lower level extb, light not in extb
 		bool const camera_in_room_part_xy(parts[room.part_id].contains_pt_xy(camera_rot));
 		bool const camera_room_same_part(room.part_id == camera_part || (is_house && camera_in_room_part_xy)); // treat stacked house parts as the same
 		bool const has_stairs_this_floor(!is_in_attic && room.has_stairs_on_floor(cur_floor));
