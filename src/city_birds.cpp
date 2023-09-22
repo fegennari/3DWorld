7e8b8e27e5c74e959f60540c0c1c00e4f5773309
Frank Gennari <fgennari@gmail.com>
2023-09-21 23:35:46 -0700

    Added bird_animated_model city config file option; Started new file city_birds.cpp

---
+++ src/city_birds.cpp
@@ -0,0 +1,11 @@
+// 3D World - City Birds Implementation
+// by Frank Gennari
+// 09/21/23
+
+#include "city_objects.h"
+
+extern int animate2;
+extern object_model_loader_t building_obj_model_loader;
+
+
+
