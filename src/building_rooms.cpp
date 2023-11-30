015efc8ba583d09741f7046f43f55d580821086f
Frank Gennari <fgennari@gmail.com>
2023-11-29 20:59:01 -0800

    Add extra building hallway lights when there are multiple stairs or elevators on that floor

--- src/building_rooms.cpp
+++ src/building_rooms.cpp
@@ -332,7 +332,20 @@ void building_t::gen_room_details(rand_gen_t &rgen, unsigned building_ix) {
 
 			if (num_lights > 1) { // r->is_hallway or ext basement
 				// hallway: place a light on each side (of the stairs if they exist), and also between stairs and elevator if there are both
-				if (r->has_elevator && r->has_stairs == 255) {max_eq(num_lights, (2U + r->has_elevator));} // main hallway with elevator + stairs on all floors; 3+ lights
+				if (r->is_hallway && r->has_elevator && r->has_stairs == 255) {
+					max_eq(num_lights, (2U + r->has_elevator)); // main hallway with elevator + stairs on all floors; 3+ lights
+					cube_t room_slice(*r), prev_elevator;
+					set_cube_zvals(room_slice, floor_zval, light_z2); // clip to the range of this floor
+					unsigned num_elevators(0), num_stairs(0);
+
+					for (elevator_t const &e : interior->elevators) {
+						if (!prev_elevator.is_all_zeros() && e.z1() == prev_elevator.z1() && e.z2() == prev_elevator.z2()) continue; // skip back-to-back elevators
+						num_elevators += room_slice.intersects(e);
+						prev_elevator  = e;
+					}
+					for (stairwell_t const &s : interior->stairwells) {num_stairs += room_slice.intersects(s);}
+					num_lights += max(num_elevators, 1U)-1 + max(num_stairs, 1U)-1; // add an extra light for each additional elevator or stairs above one
+				}
 				min_eq(num_lights, 6U);
 				float const offsets[6] = {0.0, -0.2, -0.3, -0.36, -0.4, -0.43}, steps[6] = {0.0, 0.4, 0.3, 0.24, 0.2, 0.172}; // indexed by num_lights-1
 				float const hallway_len(r->get_sz_dim(light_dim));
