c3917a1c761dfd5e8178087eb3648c4baf00253c
Frank Gennari <fgennari@gmail.com>
2024-02-01 23:59:28 -0800

    Changed building config two_floor_retail to a probability, left at 0.0 for now

--- src/building_geom.cpp
+++ src/building_geom.cpp
@@ -292,7 +292,7 @@ void building_t::gen_geometry(int rseed1, int rseed2) {
 				parts[1].z1() = parts[0].z2();
 			}
 			else if (height > 2.5*floor_spacing && rgen.rand_probability(global_building_params.retail_floorplan_prob)) { // 3+ floors
-				retail_floor_levels = (global_building_params.two_floor_retail ? 2 : 1);
+				retail_floor_levels = (rgen.rand_probability(global_building_params.two_floor_retail_prob) ? 2 : 1);
 				parts.push_back(base);
 				parts[0].z2() = parts[1].z1() = base.z1() + retail_floor_levels*floor_spacing; // split in Z: parts[0] is the bottom, parts[1] is the top
 			}
--- src/city_building_params.cpp
+++ src/city_building_params.cpp
@@ -183,9 +183,9 @@ void building_params_t::init_kw_maps() {
 	kwmb.add("tt_only", tt_only);
 	kwmb.add("infinite_buildings", infinite_buildings);
 	kwmb.add("add_secondary_buildings", add_secondary_buildings);
-	kwmb.add("add_office_basements", add_office_basements);
-	kwmb.add("put_doors_in_corners", put_doors_in_corners);
-	kwmb.add("two_floor_retail",     two_floor_retail);
+	kwmb.add("add_office_basements",    add_office_basements);
+	kwmb.add("put_doors_in_corners",    put_doors_in_corners);
+	kwmr.add("two_floor_retail_prob",   two_floor_retail_prob, FP_CHECK_01);
 	kwmr.add("split_prob", cur_mat.split_prob, FP_CHECK_01);
 	kwmr.add("cube_prob",  cur_mat.cube_prob,  FP_CHECK_01);
 	kwmr.add("round_prob", cur_mat.round_prob, FP_CHECK_01);
