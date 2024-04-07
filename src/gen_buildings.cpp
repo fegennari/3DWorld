230ae02b259e41e13c3fcaeb3229dcde9660b079
Frank Gennari <fgennari@gmail.com>
2024-04-06 22:09:23 -0700

    Changed window clipping logic to test mat.add_windows rather than mat.no_city, in case future city office buildings have window cuts added

--- src/gen_buildings.cpp
+++ src/gen_buildings.cpp
@@ -2018,8 +2018,7 @@ void building_t::get_all_drawn_window_verts(building_draw_t &bdraw, bool lights_
 		float const tint(0.2*fract(100.0f*(bcube.x1() + bcube.y1())));
 		color = colorRGBA((1.0 - tint), (1.0 - tint), (0.8 + tint), 1.0);
 	} else {color = mat.window_color;}
-	// only clip non-city windows; city building windows tend to be aligned with the building textures (maybe should be a material option?)
-	int const clip_windows(mat.no_city ? (is_house ? 2 : 1) : 0);
+	int const clip_windows(mat.add_windows ? (is_house ? 2 : 1) : 0);
 	float const floor_spacing(get_window_vspace());
 	float const gf_door_ztop(doors.empty() ? 0.0f : (EXACT_MULT_FLOOR_HEIGHT ? (ground_floor_z1 + floor_spacing) : doors.front().pts[2].z));
 	unsigned draw_parts_mask(0);
