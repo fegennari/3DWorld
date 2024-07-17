33c714a927c32d16dc3202b057a658467ec85f9c
Frank Gennari <fgennari@gmail.com>
2024-07-16 23:14:18 -0700

    Don't spawn building people in rooms with a swimming pool

--- src/building_navigation.cpp
+++ src/building_navigation.cpp
@@ -1738,10 +1738,11 @@ bool building_t::place_people_if_needed(unsigned building_ix, float radius, vect
 	for (auto r = interior->rooms.begin(); r != interior->rooms.end(); ++r) { // add room_cands
 		if (r->is_sec_bldg) continue; // don't place people in garages and sheds
 		if (min(r->dx(), r->dy()) < 3.0*radius) continue; // room to small to place a person
+		unsigned const room_ix(r - interior->rooms.begin());
+		if (interior->pool.valid && (int)room_ix == interior->pool.room_ix) continue; // don't place in pool room so that we don't have to check for pool collisions
 		unsigned const num_floors(calc_num_floors_room(*r, window_vspacing, floor_thickness));
 		assert(num_floors > 0);
 		if (first_basement_room == 0 && r->z1() < ground_floor_z1) {first_basement_room = room_cands.size();}
-		unsigned const room_ix(r - interior->rooms.begin());
 		vect_door_stack_t const &doorways(get_doorways_for_room(*r, 0.0, 1)); // get interior doors; all_floors=1
 
 		for (unsigned f = 0; f < num_floors; ++f) {
