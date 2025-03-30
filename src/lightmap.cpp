fe52772f4d30d86d6f20d1660f2b3e19410551d5
Frank Gennari <fgennari@gmail.com>
2025-03-30 01:05:15 -0700

    Attempting to fix player flashlight shadow acne in buildings far from the origin; Tried adjusting dlight shadow map bias, but that doesn't work well and is not used; Disabled player flashlight shadow when > 25 units away from the origin

--- src/lightmap.cpp
+++ src/lightmap.cpp
@@ -1100,7 +1100,9 @@ light_source get_player_flashlight_light_source(float radius_scale) {
 
 void add_player_flashlight_light_source(float radius_scale) { // for buildings
 	dl_sources.push_back(get_player_flashlight_light_source(radius_scale));
-	if (camera_in_building) {dl_sources.back().assign_smap_mgr_id(1);} // needed for building interiors
+	// flashlight shadows have problems with shadow acne when far from the origin, so disable them in this case
+	bool const enable_shadows(camera_in_building && (camera_pos - get_camera_coord_space_xlate()).mag() < 25.0);
+	if (enable_shadows) {dl_sources.back().assign_smap_mgr_id(1);} // needed for building interiors
 	else {dl_sources.back().disable_shadows();} // shadows not needed
 }
 
