0afab904743df6e96d7c1d5662e721d67f7ef45e
Frank Gennari <fgennari@gmail.com>
2025-04-08 21:06:03 -0700

    Added timing profiler track_not_print mode for high count operations

--- src/profiler.cpp
+++ src/profiler.cpp
@@ -14,42 +14,40 @@ int omp_get_thread_num_3dw();
 template <typename T> class timing_profiler {
 
 	struct entry_t {
-		unsigned count;
-		T time, tmax;
-		entry_t() : count(0), time(0), tmax(0) {}
+		unsigned count=0;
+		T time=0, tmax=0;
 		void add(T t) {++count; time += t; tmax = max(tmax, t);}
 	};
 	map<string, entry_t> entries;
 
 public:
-	bool enabled;
+	bool enabled=0;
 
-	timing_profiler() : enabled(0) {}
 	void clear() {entries.clear();}
 
-	void register_time(const char *str, T delta_time, bool no_loading_screen) {
+	void register_time(const char *str, T delta_time, bool no_loading_screen, bool track_not_print=0) {
 #pragma omp critical(timer_update)
 		{
-			if (enabled) {entries[str].add(delta_time);}
+			if (enabled || track_not_print) {entries[str].add(delta_time);}
 			else {cout << str << " time = " << delta_time << endl;}
 		}
 		if (!no_loading_screen && delta_time > 0 && omp_get_thread_num_3dw() == 0) {maybe_update_loading_screen(str);} // only call on main thread when time has elapsed
 	}
 	void stats() const {
 		if (entries.empty()) return;
-		cout << "name count total max average" << endl;
+		cout << "# name count total max average" << endl;
 		unsigned max_name(0);
 		for (auto i = entries.begin(); i != entries.end(); ++i) {max_name = max(max_name, (unsigned)i->first.size());}
 
 		for (auto i = entries.begin(); i != entries.end(); ++i) {
 			string const spaces((max_name - i->first.size()), ' ');
 			cout << i->first << spaces << ": " << i->second.count << "\t" << i->second.time << "\t"
-					<< i->second.tmax << "\t" << float(i->second.time)/float(i->second.count) << endl;
+				 << i->second.tmax << "\t" << float(i->second.time)/float(i->second.count) << endl;
 		}
 	}
 };
 
-timing_profiler<int> global_profiler;
+timing_profiler<int  > global_profiler;
 timing_profiler<float> global_highres_profiler;
 
 void toggle_timing_profiler() {global_profiler.enabled ^= 1; global_highres_profiler.enabled ^= 1;}
@@ -65,7 +63,7 @@ void timing_profiler_stats() {
 void highres_timer_t::end() {
 	if (!enabled || name.empty()) return;
 	float const elapsed(get_delta_secs(clock.now(), timer1));
-	global_highres_profiler.register_time(name.c_str(), 1000.0*elapsed, no_loading_screen); // print in ms
+	global_highres_profiler.register_time(name.c_str(), 1000.0*elapsed, no_loading_screen, track_not_print); // print in ms
 	name.clear(); // make sure we don't double count this
 }
 
--- src/profiler.h
+++ src/profiler.h
@@ -10,12 +10,12 @@ using namespace std::chrono;
 
 class highres_timer_t { // should this share a base class with timer_t?
 	std::string name;
-	bool enabled, no_loading_screen;
+	bool enabled, no_loading_screen, track_not_print;
 	high_resolution_clock::time_point timer1;
 	high_resolution_clock clock;
 public:
-	highres_timer_t(char const *const name_,  bool enabled_=1, bool nls=0) : name(name_), enabled(enabled_), no_loading_screen(nls), timer1(clock.now()) {}
-	highres_timer_t(std::string const &name_, bool enabled_=1, bool nls=0) : name(name_), enabled(enabled_), no_loading_screen(nls), timer1(clock.now()) {}
+	highres_timer_t(char const *const name_,  bool enabled_=1, bool nls=0, bool tnp=0) : name(name_), enabled(enabled_), no_loading_screen(nls), track_not_print(tnp), timer1(clock.now()) {}
+	highres_timer_t(std::string const &name_, bool enabled_=1, bool nls=0, bool tnp=0) : name(name_), enabled(enabled_), no_loading_screen(nls), track_not_print(tnp), timer1(clock.now()) {}
 	~highres_timer_t() {end();}
 	void end();
 };
