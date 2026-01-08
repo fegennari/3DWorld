93c38d30341b7ec162959129ba3dba4fc5edbf31
Frank Gennari <fgennari@gmail.com>
2026-01-07 23:04:33 -0800

    Replaced sphere_mat_t constructor with member variable initializations

--- src/sphere_materials.h
+++ src/sphere_materials.h
@@ -6,14 +6,12 @@
 #pragma once
 
 struct sphere_mat_t {
-	bool shadows, emissive, reflective;
-	int destroyable, tid, nm_tid;
-	float radius_scale, alpha, metal, spec_mag, shine, hardness, density, light_atten, refract_ix, light_radius;
-	colorRGB diff_c, spec_c;
+	bool shadows=0, emissive=0, reflective=0;
+	int destroyable=0, tid=-1, nm_tid=-1;
+	float radius_scale=1.0, alpha=1.0, metal=1.0, spec_mag=0.0, shine=1.0, hardness=0.8, density=1.0, light_atten=0.0, refract_ix=1.0, light_radius=0.0;
+	colorRGB diff_c=WHITE, spec_c=WHITE;
 	std::string name;
 
-	sphere_mat_t() : shadows(0), emissive(0), reflective(0), destroyable(0), tid(-1), nm_tid(-1), radius_scale(1.0), alpha(1.0), metal(1.0),
-		spec_mag(0.0), shine(1.0), hardness(0.8), density(1.0), light_atten(0.0), refract_ix(1.0), light_radius(0.0), diff_c(WHITE), spec_c(WHITE) {}
 	std::string get_name() const;
 };
 
