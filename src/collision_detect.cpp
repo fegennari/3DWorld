4d111701ce25ea6bac578a1d94c65080eaabab18
Frank Gennari <fgennari@gmail.com>
2026-08-22 22:17:56 -0700

    Removed a commented out block of code from vert_coll_detector::check_cobj_intersect() cylinder case that's old and I don't remember what it did

--- src/collision_detect.cpp
+++ src/collision_detect.cpp
@@ -1304,24 +1304,8 @@ void vert_coll_detector::check_cobj_intersect(int index, bool enable_cfs, bool p
 				}
 				radius += o_radius;
 				if (!safe_norm_div(rad, radius, norm)) break;
-				//float const objz(obj.pos.z);
 				norm.assign((pos.x - center.x)/rad, (pos.y - center.y)/rad, 0.0);
 				for (unsigned d = 0; d < 2; ++d) {obj.pos[d] = center[d] + norm[d]*radius;}
-
-				/*if (objz > (zmaxc - o_radius) && objz < zmaxc) { // hit on the top edge
-					obj.pos.x -= norm.x*o_radius;
-					obj.pos.y -= norm.y*o_radius;
-					norm.z += (objz - (zmaxc - o_radius))/o_radius; // denominator isn't exactly correct
-					norm.normalize();
-					obj.pos += norm*o_radius;
-				}
-				else if (objz < (zminc + o_radius) && objz > zminc) { // hit on the bottom edge
-					obj.pos.x -= norm.x*o_radius;
-					obj.pos.y -= norm.y*o_radius;
-					norm.z -= ((zminc + o_radius) - objz)/o_radius; // denominator isn't exactly correct
-					norm.normalize();
-					obj.pos += norm*o_radius;
-				}*/
 			}
 		}
 		break;
